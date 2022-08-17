4e84fd6221475f11f34266bb7c9feb814e357e43
David Morris <gm3dmo@github.com>
2022-08-17 07:53:54 +0100

    Changing commit scripts for test-data directory change.

--- create-pull-request.sh
+++ create-pull-request.sh
@@ -3,13 +3,13 @@
 # https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
 # POST /repos/:owner/:repo/pulls
 
-json_file="create-pull-request.json"
+json_file=/tmp/create-pull-request.json
 rm -f ${json_file}
 
 timestamp=$(date +%s)
 title="Amazing new feature PR: $timestamp"
 
-lorem_file="lorem-pull-request.md"
+lorem_file=/tmp/lorem-pull-request.md
 lorem_text=$(cat $lorem_file)
 
 
