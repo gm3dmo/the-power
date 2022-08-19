387424c914c7a6f9dbde7a516be749580e147946
David Morris <gm3dmo@github.com>
2022-08-19 12:00:17 +0100

    Adding repo runs.

--- list-workflow-runs.sh
+++ list-workflow-runs.sh
@@ -3,7 +3,17 @@
 # https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
 # GET /repos/:owner/:repo/actions/workflows/:workflow_id/runs
 
-workflow_id=1
+
+if [ -z "$1" ]
+  then
+    
+    workflow_id=$(./list-repository-workflows.sh | jq '[.workflows[].id] | max')
+  else
+    workflow_id=$1
+fi
+
+
+
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
