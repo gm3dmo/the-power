cd54dd71918d7d9289dd5a70f58ebfdc4a4fa0b3
David Morris <gm3dmo@github.com>
2022-08-19 11:39:40 +0100

    Adding new documentation for list workflow runs.

--- list-workflow-runs.sh
+++ list-workflow-runs.sh
@@ -1,6 +1,6 @@
 . .gh-api-exmples.conf
 
-# https://developer.github.com/v3/actions/workflow-runs/#list-workflow-runs
+# https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
 # GET /repos/:owner/:repo/actions/workflows/:workflow_id/runs
 
 workflow_id=1
