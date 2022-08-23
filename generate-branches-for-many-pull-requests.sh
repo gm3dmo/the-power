bc53437818b8ec1103577a46972fb6f4dade9a1a
David Morris <gm3dmo@github.com>
2022-08-23 08:53:37 +0100

    Adding a little more explanation to generate-many branches.

--- generate-branches-for-many-pull-requests.sh
+++ generate-branches-for-many-pull-requests.sh
@@ -1,4 +1,5 @@
 . ../../.gh-api-examples.conf
+
 # You will need to do this in a checked out repository
 # about 10000 is the biggest batch I could create.
 # Clean the file /tmp/head_branches for each run:
@@ -16,9 +17,10 @@
 # git add file_00000
 # git commit -m "adding file: file_00000"
 # echo  branch_00000 >> /tmp/head_branches
-# git checkout·
+# git checkout
 
 export base_branch
+
 
 perl  -e '$date = time(); for ("7100".."7105") { printf "git branch branch_%05d\ngit checkout branch_%05d\nfor commit in {1..50}\ndo\n   echo %05d_\${commit} > file_%05d_\${commit}\n   git add file_%05d_\${commit}\n   git commit -m \"adding file: file_%05d_\${commit}\"\ndone\necho  branch_%05d >> /tmp/head_branches\n\ngit checkout $ENV{base_branch}\n",$_,$_,$_,$_,$_,$_,$_ }' >/tmp/longlistofbranches.txt
 echo "bash /tmp/longlistofbranches.txt"
