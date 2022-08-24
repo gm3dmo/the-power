35a539d9843fed3519ab6475a388f9b00464f5da
David Morris <gm3dmo@github.com>
2022-08-24 11:19:39 +0100

    Adding enable/disable lfs scripts.

---
+++ enable-lfs.sh
@@ -0,0 +1,19 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/repos/lfs#enable-git-lfs-for-a-repository
+# PUT /repos/{owner}/{repo}/lfs
+
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+
+set -x
+curl -v ${curl_custom_flags} \
+     -X PUT \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/lfs
