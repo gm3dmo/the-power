780461d7debcca566e4958e09b2ce1d79e8ee5e1
David Morris <gm3dmo@github.com>
2022-08-24 12:57:22 +0100

    Adding graphql list organization repositories example.

---
+++ graphql-list-organization-repositories.sh
@@ -0,0 +1,45 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/graphql/reference/objects#package
+
+org=${1:-$org}
+
+graphql_query=tmp/graphql_query.txt
+rm -f ${graphql_query}
+
+cat <<EOF >$graphql_query
+{
+  organization(login: "$org") {
+		name
+    repositories(first: 100) {
+        nodes {
+            name
+            description
+            url
+            createdAt
+            updatedAt
+            pushedAt
+           }
+   }
+  }
+}
+EOF
+
+cat >&2 $graphql_query
+
+json_file=tmp/graphql-payload.json
+rm -f ${json_file}
+
+jq -n \
+  --arg graphql_query "$(cat $graphql_query)" \
+  '{query: $graphql_query}' > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d @${json_file} | jq
+
+rm -f ${graphql_query}
+rm -f ${json_file}
