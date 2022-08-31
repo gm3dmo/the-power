0cc394ba3c4c22974ab9259ee3cd98b450655081
David Morris <gm3dmo@github.com>
2022-08-31 04:51:00 +0100

    Adding gh command line tool demo

--- gh-pagination-debug-graphql.sh
+++ gh-pagination-debug-graphql.sh
@@ -1,7 +1,10 @@
 . .gh-api-examples.conf
 
 # https://cli.github.com/manual/gh_api
+# Demonstrates pagination for gh api
+# Demonstrates debug mode for gh tool
 
+export GH_DEBUG="api gh pr list"
 
 gh api graphql --paginate -F owner="${org}"  -f query='
 query($owner: String!, $endCursor: String) {
