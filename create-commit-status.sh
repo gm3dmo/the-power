bcf9e1f36577ddd7b42d33001d7e3106bcdfa0f8
David Morris <gm3dmo@github.com>
2022-09-01 23:38:53 +0100

    Adding a thing to make optional check name.

--- create-commit-status.sh
+++ create-commit-status.sh
@@ -3,12 +3,30 @@
 # https://docs.github.com/en/rest/reference/repos#create-a-commit-status
 # POST /repos/:owner/:repo/statuses/:sha
 
+
 if [ -z "$1" ]
   then
-    serial_number=$(python -c 'import time; print(int(time.time() * 1000))')
+    status_context="ci-test/this-check-is-required"
   else
-    serial_number=$1
+    status_context="ci-test/this-check-is-optional"
 fi
+
+
+json_file=tmp/skeleton.json
+
+jq -n \
+           --arg name "${repo}" \
+           '{
+             name : $name,
+           }' > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL} --data @${json_file}
+
+
 
 target_branch=${branch_name}
 
@@ -17,13 +35,14 @@ sha=$(curl --silent -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API_BASE_
 status="success"
 timestamp=$(date +%s)
 
+
 json_file=tmp/create-commit-status.json
 
 jq -n \
         --arg state "${status}" \
         --arg target_url "https://example.com/build/status" \
         --arg description "The build status was: $status This is completely fake. The status ran at: ${timestamp}" \
-        --arg context "fake-ci/jenkins_${serial_number}" \
+        --arg context "${status_context}" \
 	'{ state : $state , target_url : $target_url, description: $description, context: $context }' > ${json_file}
 
 json_string=$(cat $json_file | jq )
