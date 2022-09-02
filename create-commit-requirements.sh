4a20cad43225559715c6d644df25e37b9db3a844
David Morris <gm3dmo@github.com>
2022-09-02 10:21:22 +0100

    Make requirements.txt file for python configurable

--- create-commit-requirements.sh
+++ create-commit-requirements.sh
@@ -3,8 +3,28 @@
 # https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
 # PUT /repos/:owner/:repo/contents/:path
 
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+base64_string=$(base64 test-data/requirements.txt_)
+content=${base64_string}
+filename_in_repo=requirements.txt
+
+json_file=tmp/create-commit-requirements.json
+
+jq -n \
+                --arg nm "${default_committer}" \
+                --arg ms  "test commit message" \
+                --arg em  "${USER}+${default_committer}@${mail_domain}" \
+                --arg ct  "${content}" \
+                '{message: $ms, "committer":{ "name" : $nm, "email": $em }, content: $ct }'  > ${json_file}
+
 curl ${curl_custom_flags} \
      -X PUT \
-     -H "Authorization: token ${GITHUB_TOKEN}" \
      -H "Accept: application/vnd.github.v3+json" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/requirements.txt --data @create-commit-requirements.json
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/${filename_in_repo} --data @${json_file}
