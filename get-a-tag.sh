eb57795b3e661797c6d6b6eafa7d695fed24bb0d
David Morris <gm3dmo@github.com>
2022-09-02 15:48:40 +0100

    Adding get a tag.

---
+++ get-a-tag.sh
@@ -0,0 +1,20 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-server@3.5/rest/git/tags#get-a-tag
+# GET /repos/{owner}/{repo}/git/tags/{tag_sha}
+
+if [ -z "$1" ]
+  then
+    tag_sha=${default_tag_sha}
+  else
+    tag_sha=$1
+fi
+
+
+set -x
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/tags/${tag_sha}
+
+
