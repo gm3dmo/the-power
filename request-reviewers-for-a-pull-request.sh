1f354296e2d3d5b72f3e7f492da324ee3e693f7d
David Morris <gm3dmo@github.com>
2022-09-08 15:58:04 +0100

    Adding request reviewers script.

---
+++ request-reviewers-for-a-pull-request.sh
@@ -0,0 +1,31 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/pulls/review-requests#request-reviewers-for-a-pull-request
+# POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+reviewer=${default_committer}
+pull_number=${default_pull_request_id}
+
+json_file=tmp/request-reviewers-for-a-pull-request.json
+
+jq -n \
+           --arg reviewer "${reviewer}" \
+           '{
+             reviewers : [ $reviewer ]
+           }' > ${json_file}
+
+set -x
+curl -v ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/pulls/${pull_number}/requested_reviewers --data @${json_file}
+
+
