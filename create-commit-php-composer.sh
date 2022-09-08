83b7458f2d3d8f7a8d74426ed2e2504547ae7a44
David Morris <gm3dmo@github.com>
2022-09-08 22:30:46 +0100

    Adding php composer.json script

---
+++ create-commit-php-composer.sh
@@ -0,0 +1,38 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+# PUT /repos/:owner/:repo/contents/:path
+
+# https://getcomposer.org/doc/01-basic-usage.md
+
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+shopt -s -o nounset
+
+
+source_file="test-data/php/composer/composer.json_"
+base64_source_file=$(base64 ${source_file})
+content=${base64_source_file}
+
+filename_in_repo="composer.json"
+comment="Adding ${filename_in_repo}"
+
+json_file=tmp/create-commit-rust-cargo-lock.json
+
+jq -n \
+                --arg nm "${default_committer}" \
+                --arg ms  "Adding a ${filename_in_repo} file for ${comment}" \
+                --arg em  "${USER}+${default_committer}@${mail_domain}" \
+                --arg ct  "${content}" \
+                '{message: $ms, "committer":{ "name" : $nm, "email": $em }, content: $ct }'  > ${json_file}
+
+curl ${curl_custom_flags} \
+     -X PUT \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/${filename_in_repo} --data @${json_file}
