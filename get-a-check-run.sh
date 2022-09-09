83934f6faee81393cf892681382fee350c5ab358
David Morris <gm3dmo@github.com>
2022-09-09 22:22:52 +0100

    Adding better variable handling.

--- get-a-check-run.sh
+++ get-a-check-run.sh
@@ -3,11 +3,13 @@
 # https://docs.github.com/en/rest/checks/runs#get-a-check-run
 # GET /repos/{owner}/{repo}/check-runs/{check_run_id} 
 
+# Check run id's can be seen at:
+# https://github.com/{owner}/{repo}/pull/2/checks
 if [ -z "$1" ]
   then
     check_run_id=${default_check_run_id}
   else
-    check_run_id=${1}
+    check_run_id=$1
 fi
 
 curl  ${curl_custom_flags} \
