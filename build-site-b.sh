2f04566042bf84d1b8e74caf64870fc324caa056
David Morris <gm3dmo@github.com>
2022-09-09 21:23:20 +0100

    Fixes #31

--- build-site-b.sh
+++
@@ -1,9 +0,0 @@
-# Create the organization
-./create-organization.sh site-b
-
-./create-team.sh closed site-b
-./create-plain-users.sh site-b
-./create-site-b-members.sh site-b
-
-./create-repo.sh testrepo-aaa internal site-b
-./create-repo.sh testrepo-bbb internal site-b
