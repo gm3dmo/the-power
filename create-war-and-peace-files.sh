f515dfbaae5068614f6ab892bdf0155f917f7709
David Morris <gm3dmo@github.com>
2022-09-11 15:41:17 +0100

    Adding updates to war and peace files.

--- create-war-and-peace-files.sh
+++ create-war-and-peace-files.sh
@@ -1,17 +1,19 @@
 . .gh-api-examples.conf
 
+### This example adds the chapters of the war and peace book as commits.
+
 python3 war-and-peace-json.py
 
 ext=py
 
 for file in $(ls tmp/wp_*.json)
 do
-fn=${file:4}
-fn=${fn#*}
+    fn=${file:4}
+    fn=${fn#*}
     curl ${curl_custom_flags} \
          -X PUT \
          -H "Accept: application/vnd.github.v3+json" \
          -H "Authorization: token ${GITHUB_TOKEN}" \
              ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/docs/${fn}.${ext} --data @tmp/${fn}
-sleep 1
+    sleep 1
 done
