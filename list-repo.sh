6241b4f536ab9d06ee8f605acc04c3274bade406
David Morris <gm3dmo@github.com>
2022-09-11 14:58:46 +0100

    Change list-repo over to owner in url to match documentation.

--- list-repo.sh
+++ list-repo.sh
@@ -1,7 +1,7 @@
 . .gh-api-examples.conf
 
 # https://docs.github.com/en/rest/reference/repos#get-a-repository
-# GET the list of repos in the organization:
+# GET /repos/{owner}/{repo}
 
 
 # If the script is passed an argument $1 use that as the name
@@ -11,7 +11,6 @@ if [ -z "$1" ]
   else
     repo=$1
 fi
-
 
 # If the script is passed an argument $2 use that as the org
 if [ -z "$2" ]
@@ -24,4 +23,4 @@ fi
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}
+        ${GITHUB_API_BASE_URL}/repos/${owner}/${repo}
