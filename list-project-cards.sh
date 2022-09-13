43b98f406b3caa444c5c2dc1cb06ad92faee9a9c
David Morris <gm3dmo@github.com>
2022-09-13 17:26:05 +0100

    Adding some legacy project calls.

---
+++ list-project-cards.sh
@@ -0,0 +1,11 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/projects/cards#list-project-cards
+# GET /projects/columns/{column_id}/cards
+
+column_id=1
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/projects/columns/${column_id}/cards 
