007ef60eb067e428871a0c7bb32b76d0e13817f0
David Morris <gm3dmo@github.com>
2022-09-13 10:54:01 +0100

    Adding graphql list organization query.

---
+++ graphql-list-organization.sh
@@ -0,0 +1,26 @@
+. .gh-api-examples.conf
+
+# 
+
+# Rest Example: list-organization.sh
+
+read -r -d '' graphql_script <<- EOF
+{
+  organization(login: "$org") {
+        name
+        description
+        url
+        createdAt
+        updatedAt
+  }
+}
+EOF
+
+graphql_script="$(echo ${graphql_script//\"/\\\"})"
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
+
