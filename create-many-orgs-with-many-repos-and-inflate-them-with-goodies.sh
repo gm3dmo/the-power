5b34a2b8d670a80001cdf0a725633ddf00044369
David Morris <gm3dmo@github.com>
2022-09-16 20:23:19 +0100

    make file names pretty.

--- create-many-orgs-with-many-repos-and-inflate-them-with-goodies.sh
+++ create-many-orgs-with-many-repos-and-inflate-them-with-goodies.sh
@@ -1,6 +1,6 @@
 ./generate-long-list-of-orgs.pl
 ./generate-long-list-of-repos.pl
-for org in $(cat tmp/longlistoforgs.txt)
+for org in $(cat tmp/long-list-of-orgs.txt)
 do
   export org
   # do an inline edit on .gh-api-examples.conf file to switch out
@@ -13,9 +13,9 @@ do
   ./add-users-to-team.sh
   ./add-maintainers-to-team.sh
   ./create-an-organization-webhook.sh
-  for repo in $(cat tmp/longlistofrepos.txt)
+  for repo in $(cat tmp/long-list-of-repos.txt)
   do
-       # shim in the reponame now same as we did earlier for the org
+       # inline edit the reponame now same as we did earlier for the org
        export repo
        perl -p -i -e 's/^repo=.*/repo=$ENV{repo}/g' .gh-api-examples.conf
        ./create-repo-testrepo.sh
