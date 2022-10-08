1e2a38979621a526bf5cfeffab919afc826740fa
Sebastianoskiojoj <114359420+Sebastianoskiojoj@users.noreply.github.com>
2022-10-08 14:00:53 +0200

    Revert "Update get-a-team-by-name.sh"

--- get-a-team-by-name.sh
+++ get-a-team-by-name.sh
@@ -1,16 +1,9 @@
 . .gh-api-examples.conf
 
-# https://docs.github.com/en/rest/teams/teams#get-a-team-by-name
-# GET /orgs/{org}/teams/{team_slug}
-
-if [ -z "$1" ]
-  then
-    team_slug=${team_slug}
-  else
-    team_slug=$1
-fi
+# https://developer.github.com/v3/teams/
+# GET /organizations/:org_id/team/:team_id
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/orgs/${org}/teams/${team_slug}
+        ${GITHUB_API_BASE_URL}/organizations/4/team/1
