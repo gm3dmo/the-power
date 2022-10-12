ec06ef7715d0d0f06993335070603fdd973bafca
David Morris <gm3dmo@github.com>
2022-10-12 08:50:55 +0100

    Adding workflow scripts.

---
+++ list-workflow-runs-for-a-repository.sh
@@ -0,0 +1,29 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
+# GET /repos/{owner}/{repo}/actions/runs
+
+
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+
+json_file=tmp/skeleton.json
+
+jq -n \
+           --arg name "${repo}" \
+           '{
+             name : $name,
+           }' > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/actions/runs
---
+++ list-workflow-runs-for-a-workflow.sh
@@ -0,0 +1,21 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-workflow
+# GET /repos/:owner/:repo/actions/workflows/:workflow_id/runs
+
+
+if [ -z "$1" ]
+  then
+    
+    workflow_id=$(./list-repository-workflows.sh | jq '[.workflows[].id] | max')
+  else
+    workflow_id=$1
+fi
+
+
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/workflows/${workflow_id}/runs
