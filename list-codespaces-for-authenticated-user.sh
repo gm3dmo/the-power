b3d80d8a2a1a12ebd7c24af6c8970a452d4ffe9f
Alex Kelly <alexjohnfranciskelly@gmail.com>
2022-10-14 10:18:31 +0100

    Add scripts for codespaces API

---
+++ list-codespaces-for-authenticated-user.sh
@@ -0,0 +1,9 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/codespaces/codespaces#list-codespaces-for-the-authenticated-user
+# GET /user/codespaces
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/user/codespaces
