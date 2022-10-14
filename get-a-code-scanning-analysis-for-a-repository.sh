fdbef83bbcff2f19aaba63d07d2c79c890ad067b
David Morris <gm3dmo@github.com>
2022-10-14 21:34:40 +0100

    Adding code scanning examples.

---
+++ get-a-code-scanning-analysis-for-a-repository.sh
@@ -0,0 +1,18 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/code-scanning#get-a-code-scanning-analysis-for-a-repository
+# GET /repos/{owner}/{repo}/code-scanning/analyses/{analysis_id}
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+   analysis_id=$(./list-code-scanning-analyses-for-a-repository.sh | jq '[.[].id] | max') 
+  else
+    analysis_id=$1
+fi
+
+set -x
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/code-scanning/analyses/${analysis_id}
