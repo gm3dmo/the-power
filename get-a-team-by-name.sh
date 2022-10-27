874e64a240fd69924b6e2ceb7532c2028eedd46e
David Morris <gm3dmo@github.com>
2022-10-27 09:08:12 +0100

    Adding a get a team by name script.

--- get-a-team-by-name.sh
+++ get-a-team-by-name.sh
@@ -3,14 +3,9 @@
 # https://docs.github.com/en/rest/teams/teams#get-a-team-by-name
 # GET /orgs/{org}/teams/{team_slug}
 
-if [ -z "$1" ]
-  then
-    team_slug=${team_slug}
-  else
-    team_slug=$1
-fi
+
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/orgs/${org}/teams/${team_slug}
+     ${GITHUB_API_BASE_URL}/orgs/${org}/teams/${team_slug}
