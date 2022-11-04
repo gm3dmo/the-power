af3ffbfe205780e188d78836f73276de77dc1671
David Morris <gm3dmo@github.com>
2022-11-04 20:22:49 +0000

    Adding workflow run attempt script.

---
+++ get-a-workflow-run-attempt.sh
@@ -0,0 +1,12 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/actions/workflow-runs#get-a-workflow-run-attempt
+# GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}
+#
+run_id=
+attempt_number=
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/actions/runs/${run_id}/attempts/${attempt_number}
