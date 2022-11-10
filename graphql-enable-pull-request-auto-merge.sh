09ca233c805dce9778105b7ffab0a46559119e96
David Morris <gm3dmo@github.com>
2022-11-10 22:01:16 +0000

    Adding graphql commands to enable auto-merge.:

---
+++ graphql-enable-pull-request-auto-merge.sh
@@ -0,0 +1,39 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/graphql/reference/mutations#createipallowlistentry
+
+set -x
+
+graphql_query=tmp/graphql_query.txt
+rm -f ${graphql_query}
+
+if [ -z "$1" ]
+  then
+   pull_request_id=2
+  else
+    pull_request_id=${default_pull_request_id}
+fi
+
+pull_request_id_graphql=$(./graphql-get-pull-request-id.sh ${pull_request_id} | jq -r '.data.repository.pullRequest.id')
+
+cat <<EOF >$graphql_query
+mutation {
+    enablePullRequestAutoMerge(input: {pullRequestId: "$pull_request_id_graphql", mergeMethod: MERGE}) {
+        clientMutationId
+         }
+}
+EOF
+
+json_file=tmp/graphql-enable-pull-request-auto-merge.json
+rm -f ${json_file}
+
+jq -n \
+  --arg graphql_query "$(cat $graphql_query)" \
+  '{query: $graphql_query}' > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d @${json_file} | jq
---
+++ graphql-get-pull-request-id.sh
@@ -0,0 +1,33 @@
+. .gh-api-examples.conf
+
+#
+
+# Credit: https://stackoverflow.com/questions/69916356/how-do-i-enable-auto-merge-on-a-github-pull-request-via-the-rest-api
+
+if [ -z "$1" ]
+  then
+    pull_request_id=${default_pull_request_id}
+  else
+    pull_request_id=$1
+fi
+
+
+read -r -d '' graphql_script <<- EOF
+{
+    repository(name: "$repo", owner: "$org") {
+        pullRequest(number: $pull_request_id) {
+                  id
+              }
+        }
+}
+EOF
+
+# Escape quotes and reformat script to a single line
+graphql_script="$(echo ${graphql_script//\"/\\\"})"
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
+
