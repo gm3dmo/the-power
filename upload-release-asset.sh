bf1c3266b8504b36255c08aea44b1a0977a68bc1
David Morris <gm3dmo@github.com>
2022-11-10 20:28:44 +0000

    Changed release asset upload to work with a timestamp.

--- upload-release-asset.sh
+++ upload-release-asset.sh
@@ -2,6 +2,13 @@
 
 # https://docs.github.com/en/rest/reference/repos#upload-a-release-asset
 # POST /repos/{owner}/{repo}/releases/{release_id}/assets
+#
+#
+# Caveat:
+# The value:
+# "upload_url": "https://uploads.github.com/repos/octocat/Hello-World/releases/1/assets{?name,label}" 
+# seems like it could be clearer.
+
 
 response_file=tmp/response.json
 response=$(curl -sH "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases/latest > ${response_file})
@@ -10,12 +17,14 @@ release_id=$(cat $response_file | jq -r '.id')
 upload_url=$(cat $response_file | jq -r '.upload_url')
 clean_upload_url=${upload_url%?????????????}
 
-upload_asset_filename=test-data/release-asset.gz
-
 set -x
+timestamp=$(date +%s)
+upload_asset_filename=test-data/release-asset.gz
+upload_asset_filename_b=release-asset-${timestamp}.gz
+
 curl  -v \
       -H "Authorization: token ${GITHUB_TOKEN}" \
       -H "Accept: application/json"  \
       -H "Content-Type: binary" \
-         "${clean_upload_url}?name=${upload_asset_filename}" --data-binary @${upload_asset_filename}
+         "${clean_upload_url}?name=${upload_asset_filename_b}" --data-binary @${upload_asset_filename}
 
