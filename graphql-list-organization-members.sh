e65ac9c431675ed6f7009b8137cc68bab5862384
David Morris <gm3dmo@github.com>
2022-11-10 22:01:47 +0000

    Adding some recent scripts from the queue.

---
+++ graphql-list-organization-members.sh
@@ -0,0 +1,29 @@
+. .gh-api-examples.conf
+
+# 
+# 
+
+
+read -r -d '' graphql_script <<- EOF
+{
+  organization(login: "$org") {
+    membersWithRole(first: 100) {
+      nodes {
+        login
+        id
+      }
+    }
+  }
+}
+EOF
+
+# Escape quotes and reformat script to a single line
+graphql_script="$(echo ${graphql_script//\"/\\\"})"
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
+
---
+++ list-deliveries-for-an-organization-webhook.sh
@@ -0,0 +1,17 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/orgs/webhooks#list-deliveries-for-an-organization-webhook
+# GET /orgs/{org}/hooks/{hook_id}/deliveries
+
+if [ -z "$1" ]
+  then
+    hook_id=$default_org_hook_id
+  else
+    hook_id=$1
+fi
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/orgs/${org}/hooks/${hook_id}/deliveries 
+
