585c1752f3fa7f99a4e5b873b2cbda053d9594c6
David Morris <gm3dmo@github.com>
2022-11-12 07:36:30 +0000

    Adding a configurable way of loading the README.md file

---
+++ create-readme-file.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+This creates a json file for upload to the GitHub REST API
+"""
+
+
+import os
+import json
+import string
+import base64
+import argparse
+import logging
+import thepower
+from pathlib import Path
+from datetime import datetime
+
+
+def main(args):
+
+    power_config = thepower.read_dotcom_config(args.power_config)
+    args.extension = power_config.get('dummy_section','file_extension').strip('"')
+    args.default_committer = power_config.get('dummy_section','default_committer',).strip('"')
+
+    p = Path('test-data/README.txt')
+    json_file = f"""tmp/README.json"""
+    filename_in_repo = f"""docs/README.md"""
+
+    with open(p, 'rb') as ct:
+       t = {}
+       chapter_content = ct.read()
+       chapter_base64 = base64.encodebytes(chapter_content)
+       t["message"] = f"""A README.md file."""
+       t["committer"] = {}
+       t["committer"]["name"] = args.default_committer
+       t["committer"]["email"] = f"noreply+{args.default_committer}@example.com"
+       t["content"] = chapter_base64.decode('UTF-8')
+       with open(json_file, 'w') as out_file:
+          out_file.write(json.dumps(t))
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--power-config", action="store", dest="power_config", default=".gh-api-examples.conf", help="This is the config file to use to access variables for the power.")
+    parser.add_argument("-e", "--extension", action="store", dest="extension", default="c")
+    args = parser.parse_args()
+
+    main(args)
