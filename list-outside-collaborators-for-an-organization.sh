39b02d52765fb86da3903e99b94b248b2478e38e
David Morris <gm3dmo@github.com>
2022-11-14 14:36:16 +0000

    Adding list outside collaborators for an org script.

---
+++ list-outside-collaborators-for-an-organization.sh
@@ -0,0 +1,10 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/orgs/outside-collaborators#list-outside-collaborators-for-an-organization
+# GET /orgs/{org}/outside_collaborators
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/orgs/${org}/outside_collaborators
