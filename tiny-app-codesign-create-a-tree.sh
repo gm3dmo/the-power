c6b499d6403a62ebf26d0d4102a8a42e290d0669
David Morris <gm3dmo@github.com>
2022-11-16 18:33:18 +0000

    Adding some testing scripts for #60.

---
+++ tiny-app-codesign-create-a-tree.sh
@@ -0,0 +1,24 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/git#create-a-tree
+# POST /repos/:owner/:repo/git/trees
+
+
+# Override the GITHUB_TOKEN in the .gh-api-examples.conf with one from a GitHub App:
+GITHUB_TOKEN=$1
+last_commit_sha=$(curl --silent -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/branches/${base_branch} | jq -r '.commit.sha')
+timestamp=$(date +%s)
+json_file=tmp/github-app-codesign-create-a-tree.json
+
+
+jq -n \
+        --arg base_tree "${last_commit_sha}" \
+        --arg path "/github-app-codesign" \
+'{ "base_tree" : $base_tree, "tree" : [ { "path": $path, "mode": "100755", type: "blob", content: $content }]}'  > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     -H "Accept: application/vnd.github.v3+json" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/trees --data @${json_file}
---
+++ tiny-app-codesign-update-a-reference.sh
@@ -0,0 +1,22 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/git#update-a-reference
+# PATCH /repos/{owner}/{repo}/git/refs/{ref}
+
+
+GITHUB_TOKEN=$1
+sha=$(cat tmp/pre-receive-new-commit-response.json | jq -r '.sha')
+json_file=tmp/create-commit.json
+
+
+jq -n \
+       --arg sha "${sha}" \
+              '{ sha: $sha }' > ${json_file}
+
+
+curl ${curl_custom_flags} \
+     -X PATCH \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs/heads/${base_branch} --data @${json_file}
+
