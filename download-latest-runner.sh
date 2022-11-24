7ac2292100c9959f4c7892cc864f2c05554328d7
David Morris <gm3dmo@github.com>
2022-11-24 17:52:52 +0000

    Adding runner scripts.

---
+++ download-latest-runner.sh
@@ -0,0 +1,14 @@
+. .gh-api-examples.conf
+
+# tag name looks like: v2.299.1
+# we use the ##v shell built-in to strip the v off that.
+
+tag_name=$(./list-runner-latest-release.sh)
+runner_version=${tag_name##v}
+
+if [ $hostname == "api.github.com" ];
+then
+  hostname="github.com"
+fi
+
+curl -O -L https://${hostname}/actions/runner/releases/download/${tag_name}/actions-runner-${runner_os}-${runner_platform}-${runner_version}.tar.gz
