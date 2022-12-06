3da08c6792b0b5e12e3f677b47c0bec36174cc23
Viorel Rudareanu <24523236+voxsteel@users.noreply.github.com>
2022-12-06 16:25:51 +0000

    Updates on graphql branch script

---
+++ graphql-update-branch-protection-rule.sh
@@ -0,0 +1,61 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/graphql/reference/mutations#createipallowlistentry
+
+if [[ -z $1 ]]; then
+ branch_protection_rule=BPR_kwDOIajZ6s4B1mUR 
+else
+ branch_protection_rule=$1 
+fi
+
+
+graphql_query=tmp/graphql_query.txt
+rm -f ${graphql_query}
+
+cat <<EOF >$graphql_query
+mutation {
+    updateBranchProtectionRule(
+        input: {
+            branchProtectionRuleId:"$branch_protection_rule",
+            pattern:"main",
+            requiresApprovingReviews:false,
+            requiredApprovingReviewCount:0,
+            dismissesStaleReviews:false,
+            requiresCodeOwnerReviews:false,
+            requiresStatusChecks:true,
+            requiresStrictStatusChecks:false,
+            requiredStatusChecks: [
+                {
+                    appId:"any",
+                    context:"abc",
+            },
+            ]
+            requiresConversationResolution:false,
+            requiresCommitSignatures:false,
+            requiresLinearHistory:false,
+            isAdminEnforced:false,
+            allowsForcePushes:false,
+            allowsDeletions:false,
+            }
+        ) {
+        clientMutationId
+    }
+}
+EOF
+
+json_file=tmp/graphql-payload.json
+rm -f ${json_file}
+
+jq -n \
+  --arg graphql_query "$(cat $graphql_query)" \
+  '{query: $graphql_query}' > ${json_file}
+
+
+curl -v ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d @${json_file} | jq
+
+rm -f ${graphql_query}
+rm -f ${json_file}
