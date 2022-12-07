a7ee36fe3976d8d06b43173d01119881ecfc7942
Your Name <you@example.com>
2022-12-07 15:28:49 +0100

    add file for generate release notes endpoint

---
+++ generate-release-notes-content-for-a-release.sh
@@ -0,0 +1,21 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-server@3.5/rest/releases/releases#generate-release-notes-content-for-a-release
+# POST /repos/{owner}/{repo}/releases/generate-notes 
+
+json_file=tmp/create-release.json
+timestamp=$(date +%s)
+previous_tag_name=""
+generate_release_notes="true"
+
+jq -n \
+        --arg tag_name "v1.0.${timestamp}" \
+              '{tag_name : $tag_name}'  > ${json_file}
+
+cat $json_file | jq -r >&2
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/releases --data @${json_file}
+
