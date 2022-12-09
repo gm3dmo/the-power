bbfa338bdd2f9d9407a1a7d246600b1b8dec9915
Tim <103634854+sn2b@users.noreply.github.com>
2022-12-09 11:10:50 +0100

    Create list-dependabot-alerts-for-an-organization.sh

---
+++ list-dependabot-alerts-for-an-organization.sh
@@ -0,0 +1,9 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/dependabot/alerts?apiVersion=2022-11-28#list-dependabot-alerts-for-an-organization
+# GET the list of dependabot alerts for an organization
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/orgs/${org}/dependabot/alerts
