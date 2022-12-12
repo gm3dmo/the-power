3bee2b66ddadbefce90ac5f75aae40636cd0e0f9
David Morris <gm3dmo@github.com>
2022-12-12 13:30:45 +0000

    Adding list fine grained repositories script.

---
+++ list-fine-grained-permissions-for-an-organization.sh
@@ -0,0 +1,18 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/custom-roles?apiVersion=2022-11-28#list-fine-grained-permissions-for-an-organization
+# GET /orgs/{org}/fine_grained_permissions
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    org=$org
+  else
+    org=$1
+fi
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/orgs/${org}/fine_grained_permissions
