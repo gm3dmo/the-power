afdc584275c8c5bdf301d9083b7efc1945d5ba4e
David Morris <gm3dmo@github.com>
2022-12-15 17:32:06 +0000

    Adding missing list script.

---
+++ list-repositories-accessible-to-the-user-access-token.sh
@@ -0,0 +1,12 @@
+.  .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/apps#remove-a-repository-from-an-app-installation
+# GET /user/installations/{installation_id}/repositories
+
+installation_id=${default_installation_id}
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Accept: application/vnd.github.machine-man-preview+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/user/installations/${installation_id}/repositories
