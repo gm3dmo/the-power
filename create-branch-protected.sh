ed3749558b74c7f205606d98b2d64ac80eb331fb
David Morris <gm3dmo@github.com>
2022-12-30 14:06:08 +0000

    Adding the flags to curl to let these scripts work for users using --insecure.

--- create-branch-protected.sh
+++ create-branch-protected.sh
@@ -14,8 +14,8 @@ fi
 timestamp=$(date +"%s")
 json_file=tmp/create-branch-protectedbranch.json
 
-sha=$(curl --silent -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs/heads/${base_branch} | jq -r '.object.sha')
-echo $sha
+
+sha=$(curl ${curl_custom_flags} --silent -H "Authorization: token ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs/heads/${base_branch} | jq -r '.object.sha')
 
 jq -n \
 	--arg ref "refs/heads/${protected_branch_name}" \
