2b5ab95b8b9e571644b6b575c73ddfd5a04bb28f
Alex Kelly <alexjohnfranciskelly@gmail.com>
2023-01-04 09:52:53 +0000

    Update set-opt-in-flag-of-OIDC-subject-claim-customization-for-a-repo.sh

--- set-opt-in-flag-of-OIDC-subject-claim-customization-for-a-repo.sh
+++ set-opt-in-flag-of-OIDC-subject-claim-customization-for-a-repo.sh
@@ -4,22 +4,29 @@
 # PUT /repos/{owner}/{repo}/actions/oidc/customization/sub
 # You must authenticate using an access token with the repo scope to use this endpoint.
 
-use_default=$1
-
-if [ "$use_default" == "true" ]
-then
-    curl ${curl_custom_flags} \
-        -X PUT \
-        -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/oidc/customization/sub -d '{"use_default":true}'
-elif [ "$use_default" == "false" ]
-then
-    curl ${curl_custom_flags} \
-        -X PUT \
-        -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/oidc/customization/sub -d '{"use_default":false}'
-else
-  echo "You must pass either 'true' or 'false' as an argument to this script."
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
 fi
+
+use_default=$2
+
+if [ "$use_default" != "true" ] && [ "$use_default" != "false" ]; then
+  echo "You must pass either 'true' or 'false' as an argument to this script."
+  exit 1
+fi
+
+json_file=tmp/set-opt-in-flag-of-an-oidc-subject-claim.json
+rm -f ${json_file}
+
+jq -n \
+  --argjson use_default true '{"use_default": $use_default}' > ${json_file}
+
+set -x
+curl ${curl_custom_flags} \
+     -X PUT \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/oidc/customization/sub -d @${json_file}
