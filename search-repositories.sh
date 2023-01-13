3ecf03bfd8de05d498398c3cc7e72486b397a786
Tim <103634854+sn2b@users.noreply.github.com>
2023-01-13 12:01:10 +0100

    Update search-repositories.sh

--- search-repositories.sh
+++ search-repositories.sh
@@ -3,7 +3,8 @@
 # https://docs.github.com/en/enterprise-cloud@latest/rest/search?apiVersion=2022-11-28#search-repositories
 # https://api.github.com/search/repositories?q=Q
 
-default_start_date="$(date -v -7d -I)"
+# WORKS ON A MAC
+default_start_date="$(date -v -7d +'%Y-%m-%d')"
 
 # Pass query as an argument, otherwise list all repos with commits in the last seven days
 if [ -z "$1" ]
