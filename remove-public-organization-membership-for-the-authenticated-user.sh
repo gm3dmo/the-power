77c8dbc2ba821e7bc62cfefdc68ed280c094dcf0
Alex Kelly <alexjohnfranciskelly@gmail.com>
2023-01-19 09:55:37 +0000

    Add script to remove public org membership

---
+++ remove-public-organization-membership-for-the-authenticated-user.sh
@@ -0,0 +1,17 @@
+. .gh-api-examples.conf
+
+# https://docs.github.com/en/rest/orgs/members?apiVersion=2022-11-28#remove-public-organization-membership-for-the-authenticated-user
+# DELETE /orgs/{org}/public_members/{username}
+
+# If the script is passed an argument $1 use that as the username
+if [ -z "$1" ]
+  then
+    username=$username
+  else
+    username=$1
+fi
+
+curl -X DELETE \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: token ${GITHUB_TOKEN}" \
+     ${GITHUB_API_BASE_URL}/orgs/${org}/public_members/${username}
