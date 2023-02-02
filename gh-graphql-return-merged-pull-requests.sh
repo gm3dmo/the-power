5339506eebe80a58e49e2d988904e65c381f811d
Tim <103634854+sn2b@users.noreply.github.com>
2023-02-02 15:55:22 +0100

    Create gh-graphql-return-merged-pull-requests.sh

---
+++ gh-graphql-return-merged-pull-requests.sh
@@ -0,0 +1,31 @@
+. .gh-api-examples.conf
+
+# https://cli.github.com/manual/gh_api
+
+gh api graphql --paginate -F owner="${org}"  -f query='
+query GetPRs($owner: String!, $endCursor: String) {
+  organization(login: $owner) {
+    repositories(first: 100) {
+      nodes {
+        name
+				id
+        pullRequests(first: 100, after:$endCursor, orderBy: {field: CREATED_AT, direction: DESC}, states: MERGED) {
+          nodes {
+            title
+            headRefName
+            bodyText
+            additions
+            deletions
+            createdAt
+            id
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+}
+'
