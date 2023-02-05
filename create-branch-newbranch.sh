1d391dc8c0408ba933556717f1bdf5f0d5190746
David Morris <gm3dmo@github.com>
2023-02-05 11:37:56 +0000

    Make the newbranch use a json tmp file.

--- create-branch-newbranch.sh
+++ create-branch-newbranch.sh
@@ -21,9 +21,8 @@ jq -n \
         --arg sha "${sha}" \
 	'{ ref: $ref, sha: $sha }' > ${json_file}
 
-json_string=$(cat $json_file | jq )
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: token ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs --data "${json_string}"
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs --data @${json_file}
