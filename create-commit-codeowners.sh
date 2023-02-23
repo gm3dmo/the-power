fe03c901d60b451900dacfe621b312b554ddbf79
David Morris <gm3dmo@github.com>
2023-02-23 20:30:23 +0000

    Adding codeowners files and moved to a template.

--- create-commit-codeowners.sh
+++ create-commit-codeowners.sh
@@ -10,19 +10,9 @@ if [ -z "$1" ]
     repo=$1
 fi
 
-raw_text="/docs/README.md @${org}/${team_slug}\n/.gitattributes @${default_committer}"
-base64_string=$(echo ${raw_text} | /usr/bin/base64)
-content=${base64_string}
-filename_in_repo=CODEOWNERS
-
+python3 create-commit-codeowners.py
+filename_in_repo="CODEOWNERS"
 json_file=tmp/create-commit-codeowners.json
-
-jq -n \
-                --arg nm "${default_committer}" \
-                --arg ms  "Create a CODEOWNERS file" \
-                --arg em  "${USER}+${default_committer}@${mail_domain}" \
-                --arg ct  "${content}" \
-                '{message: $ms, "committer":{ "name" : $nm, "email": $em }, content: $ct }'  > ${json_file}
 
 curl ${curl_custom_flags} \
      -X PUT \
