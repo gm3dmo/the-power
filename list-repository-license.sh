3e18a29d65ed80b8961e8c8a4455d6d947d37cd8
David Morris <gm3dmo@github.com>
2023-02-24 06:57:30 +0000

    Adding new url to comment.

--- list-repository-license.sh
+++ list-repository-license.sh
@@ -1,17 +1,15 @@
 . .gh-api-examples.conf
 
-# https://docs.github.com/en/enterprise-server@2.20/rest/reference/licenses#get-the-license-for-a-repository
+# https://docs.github.com/en/enterprise-cloud@latest/rest/licenses?apiVersion=2022-11-28#get-a-license
 # GET /repos/{owner}/{repo}/license
 
-# If the script is passed an argument $1 use that as the name
+# If the script is passed an argument $1 use that as the repo
 if [ -z "$1" ]
   then
     repo=$repo
   else
     repo=$1
 fi
-
-set -x
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
