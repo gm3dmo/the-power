7fda83a8c46b8e087ad7b4a7bf03d3980b21aeb9
David Morris <gm3dmo@github.com>
2023-03-03 09:34:21 +0000

    Adding agent decoration to add-many-users-to-org

--- add-many-users-to-org.sh
+++ add-many-users-to-org.sh
@@ -3,9 +3,11 @@
 # https://docs.github.com/en/rest/reference/orgs#set-organization-membership-for-a-user
 # PUT /orgs/:org/memberships/:username
 
+TESTNAME="add-mny-usrs"
+
 for person in $(cat tmp/longlistofpeople.txt)
 do
-  curl ${curl_custom_flags} \
+  curl --user-agent ${TESTNAME}-${person} ${curl_custom_flags} \
        -X PUT \
        -H "Accept: application/vnd.github.v3+json" \
        -H "Authorization: token ${GITHUB_TOKEN}" \
