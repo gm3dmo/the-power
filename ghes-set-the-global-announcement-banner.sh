74b2dc4b777a4df7ad7b52cf639b718cbb3dc835
David Morris <gm3dmo@github.com>
2023-03-21 14:10:54 +0000

    Fixing the ghes banner files.

--- ghes-set-the-global-announcement-banner.sh
+++ ghes-set-the-global-announcement-banner.sh
@@ -9,15 +9,15 @@ json_file=tmp/set-the-global-announcement-banner.json
 
 
 jq -n \
-           --arg announcement $announcement \
+           --arg announcement "${announcement}" \
            '{
-             announcement: $name,
+             announcement : $announcement
            }' > ${json_file}
 
 
 curl ${curl_custom_flags} \
-     -X PATCH
+     -X PATCH \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}  --data @${json_file}
+        ${GITHUB_API_BASE_URL}/enterprise/announcement  --data @${json_file}
