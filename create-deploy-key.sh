2e4f8e4a6351b4c068ef37441bc413a8e5a81983
David Morris <gm3dmo@github.com>
2023-03-23 17:50:03 +0000

    Deploy key update.

--- create-deploy-key.sh
+++ create-deploy-key.sh
@@ -2,25 +2,22 @@
 
 # https://docs.github.com/en/rest/reference/repos#create-a-deploy-key
 # POST /repos/{owner}/{repo}/keys
+#
+set -x
 
 public_key_file=${1:-${my_ssh_pub_key}}
 public_key=$(cat ${public_key_file})
 ts=$(date +%s)
 
-json=tmp/keyDetails
-rm -f ${json}
+json_file=tmp/create-a-deploy-key.json
 
-DATA=$(jq -n \
+jq -n \
               --arg title "The Key ${ts}" \
               --arg key "${public_key}" \
-                    '{title: $title, key: $key}' ) 
-
-echo ${DATA} > ${json}
+                    '{title: $title, key: $key}' > ${json_file}
 
 curl ${curl_custom_flags} \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Content-Type: application/json" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/keys --data @${json}
-
-rm -f ${json}
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/keys --data @${json_file}
