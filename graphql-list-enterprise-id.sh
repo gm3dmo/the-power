99117a04e7b6c0b3ddff4ed6a7d35957d61bedd5
David Morris <gm3dmo@github.com>
2023-03-31 15:04:07 +0100

    update doc to use global enterprise id.

--- graphql-list-enterprise-id.sh
+++ graphql-list-enterprise-id.sh
@@ -17,8 +17,15 @@ EOF
 graphql_script="$(echo ${graphql_script//\"/\\\"})"
 
 
+# https://docs.github.com/en/graphql/guides/migrating-graphql-global-node-ids
+# To facilitate migration to the new ID format, you can use the X-Github-Next-Global-ID header in your GraphQL API requests. The value of the X-Github-Next-Global-ID header can be 1 or 0. Setting the value to 1 will force the response payload to always use the new ID format for any object that you requested the id field for. Setting the value to 0 will revert to default behavior, which is to show the legacy ID or new ID depending on the object creation date.
+# Setting this to 0:
+#      -H "X-Github-Next-Global-ID: 1" \
+# will return the legacy id.
+
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
+     -H "X-Github-Next-Global-ID: 1" \
      -H 'Accept: application/vnd.github.audit-log-preview+json' \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
