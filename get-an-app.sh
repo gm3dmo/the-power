35df55787ed49d27fe9146472c8b9001d5e05d75
David Morris <gm3dmo@github.com>
2023-04-12 15:51:30 +0100

    Adding changes to the get-an-app script.

--- get-an-app.sh
+++ get-an-app.sh
@@ -2,6 +2,10 @@
 
 # https://docs.github.com/en/enterprise-cloud@latest/rest/apps/apps?apiVersion=2022-11-28#get-an-app
 # GET /apps/{app_slug}
+#
+# This has different behaviour for authentication between
+# GHES and GHEC:
+# https://docs.github.com/en/enterprise-server@3.8/rest/apps/apps
 
 
 # If the script is passed an argument $1 use that as the name
@@ -9,11 +13,17 @@ if [ -z "$1" ]
   then
     echo "Please provide app name as parameter after the script like ./get-an-app.sh your-app-name" && exit
   else
-    app_name=$1
+    app_slug=$1
 fi
 
-curl \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_TOKEN}"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/apps/${app_name}
+
+# This version is set up to work on GHES:
+JWT=$(./tiny-call-get-jwt.sh 2>/dev/null)
+
+set -x
+curl ${curl_custom_flags} \
+     -H "Authorization: Bearer ${JWT}" \
+     -H "Accept: application/vnd.github.machine-man-preview+json" \
+        ${GITHUB_API_BASE_URL}/app/installations
+
+
