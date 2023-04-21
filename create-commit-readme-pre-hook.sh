5198dfd895efd4bc0205d300fff6236f95bafebd
David Morris <gm3dmo@github.com>
2023-04-21 22:04:51 +0100

    Fixing pre-receive hoooks to look nice.

---
+++ create-commit-readme-pre-hook.sh
@@ -0,0 +1,15 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+# PUT /repos/:owner/:repo/contents/:path
+#
+
+python3 create-commit-readme-pre-hook.py
+
+repo=${pre_receive_hook_repo}
+
+curl ${curl_custom_flags} \
+     -X PUT \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/docs/README.md" --data @tmp/create-commit-readme.json
