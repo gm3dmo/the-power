173cb9b3a8950ab01acb3e79976a810fd88cd59a
David Morris <gm3dmo@github.com>
2023-05-24 07:57:18 +0100

    Adding graphql-get-repo-size.sh

---
+++ graphql-get-repo-size.sh
@@ -0,0 +1,20 @@
+.  ./.gh-api-examples.conf
+
+# Wrap a graphql script for use with curl
+
+# Use a bash "here" document and shell variables will be available:
+
+read -r -d '' graphql_script <<- EOF
+{ repository(owner: "$org", name: "$repo") { diskUsage name} } 
+EOF
+
+# Escape quotes and reformat script to a single line
+graphql_script="$(echo ${graphql_script//\"/\\\"})"
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
+
