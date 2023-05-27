8d813cf44d1355156d787f4b0741415c02c03726
David Morris <gm3dmo@github.com>
2023-05-27 12:32:03 +0100

    Adding support for api.github.local

--- configure.py
+++ configure.py
@@ -311,7 +311,7 @@ pool_size=10
     else:
         args.hostname = input(f"Enter GitHub hostname: ")
 
-    if args.hostname == "api.github.com":
+    if args.hostname == "api.github.com" or args.hostname == "api.github.local":
         args.path_prefix = ""
         args.graphql_path_prefix = "/graphql"
     else:
