076d68c9da41819d6217fd93a7d636ef4820acf3
David Morris <gm3dmo@github.com>
2023-06-28 08:41:32 +0100

    Adding default true for repo has discussions. See #106

--- create-repo-testrepo.sh
+++ create-repo-testrepo.sh
@@ -13,9 +13,10 @@ DATA=$(jq -n \
                   --arg has_issues true \
                   --arg has_projects true \
                   --arg has_wiki true \
+                  --arg has_discussions true \
                   --arg description "This is: ${repo}, it's a private repo.  You can look at the hooks: ${webhook_url} if that helps." \
                   --arg allow_auto_merge "${allow_auto_merge}" \
-                  '{name : $name, description: $description, private: $pr | test("true"), allow_auto_merge: $allow_auto_merge, has_issues: $has_issues, has_projects: $has_projects, has_wiki: $has_wiki }' )
+                  '{name : $name, description: $description, private: $pr | test("true"), allow_auto_merge: $allow_auto_merge, has_issues: $has_issues, has_projects: $has_projects, has_wiki: $has_wiki, has_discussions:$has_discussions }' )
 
 echo ${DATA} > ${json_file}
 
