2bdf9e16df359df0dff2cecb2f3237211fc041ae
David Morris <gm3dmo@github.com>
2023-07-19 17:01:33 +0100

    Adding proper url.

--- list-pull-requests-with-commit.sh
+++ list-pull-requests-with-commit.sh
@@ -1,12 +1,11 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/repos#list-pull-requests-associated-with-a-commit
+# https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-pull-requests-associated-with-a-commit
 # GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls
 
-# gnarly hardcoded commit sha or $1
 commit_sha=$1
 
-curl ${curl_custom_flags} \
+curl -v ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Accept: application/vnd.github.groot-preview+json" \
      -H "Accept: application/vnd.github.shadow-cat-preview+json" \
