c3b2b926c245a1827a666d605d318275fe50eadf
David Morris <gm3dmo@github.com>
2023-07-20 10:03:15 +0100

    Adding copilot for business scripts.

---
+++ get-copilot-for-business-seat-information-and-settings-for-an-organization.sh
@@ -0,0 +1,11 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/copilot/copilot-for-business?apiVersion=2022-11-28#get-copilot-for-business-seat-information-and-settings-for-an-organization
+# GET /orgs/{org}/copilot/billing
+
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/orgs/{org}/copilot/billing"
