2da275a88f6904791566ff90224da8d65552ff48
David Morris <gm3dmo@github.com>
2023-07-20 22:00:55 +0100

    Remove debug flags.

--- ghes-get-all-ghes-node-metadata-for-all-nodes.sh
+++ ghes-get-all-ghes-node-metadata-for-all-nodes.sh
@@ -3,7 +3,6 @@
 # https://docs.github.com/en/enterprise-server@3.9/rest/enterprise-admin/manage-ghes?apiVersion=2022-11-28#get-all-ghes-node-metadata-for-all-nodes
 # GET /manage/v1/config/nodes
 
-set -x
-curl -v ${curl_custom_flags} \
+curl ${curl_custom_flags} \
         -u "api_key:${mgmt_password}" \
         "https://${hostname}:${mgmt_port}/manage/v1/config/nodes"
