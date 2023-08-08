265c4576b6df52fc04194c54cde8a2aeafd31e63
David Morris <gm3dmo@github.com>
2023-08-08 15:46:03 +0100

    Adding a google api secret.

--- test-commit-secret.sh
+++ test-commit-secret.sh
@@ -4,7 +4,12 @@ cd src/${repo}
 # in a way that will set off a false positive (hopefully).
 gh1=ghp_9
 gh2=gDO8kvREKK9toy7CsUunZEY77XvGA1pNv5F
+
+google_api1="AIzaSyDxJB-"
+google_api2="2ocxX02LIAZiU_2_0bhhc2wDlN8g"
+
 echo export GITHUB_TOKEN=${gh1}${gh2} >github.token.compromised.secret.txt
+echo "${google_api1}${google_api2}"   >google_api_key.compromised.secret.txt
 
 git add *.txt
 git commit -m "Adding all the keys."
