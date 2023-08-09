21a398cafb1d010afec89ee33d0b47b68888d34d
David Morris <gm3dmo@github.com>
2023-08-09 09:58:59 +0100

    Adding npm_granular token.

--- test-commit-secret.sh
+++ test-commit-secret.sh
@@ -1,8 +1,7 @@
 
 .  ./.gh-api-examples.conf
 
-This script is intended to run after build-testcase-secret-scanning
-
+# This script is intended to run after build-testcase-secret-scanning
 
 cd src/${repo}
 # We split the token in 2 so that it's never checked in to the power
@@ -11,33 +10,46 @@ cd src/${repo}
 # Breaking tokens up to get past a secret scanner is a bad thing
 # it will likely get you fired.
 
-gh1=ghp_9
-gh2=gDO8kvREKK9toy7CsUunZEY77XvGA1pNv5F
 
-google_api1="AIzaSyDxJB-"
-google_api2="2ocxX02LIAZiU_2_0bhhc2wDlN8g"
-
-azure_storage_1="e6ZZx75Z6095KHJvBZIDOD9kCLt3KjHx/"
-azure_storage_2="KKT0LWi25F+Cq0XxlpS+tbs1EqlqvRY3YgC19T5+PC6+AStjkFjyQ=="
+if [ -z "$1" ]
+  then
+    keyname=all
+  else
+    keyname=$1
+fi
 
 
 
 function github_pat () { 
-    echo "Commit: GITHUB PAT"
+    gh1=ghp_9
+    gh2=gDO8kvREKK9toy7CsUunZEY77XvGA1pNv5F
+    echo "Commit: GitHub PAT"
     echo export GITHUB_TOKEN=${gh1}${gh2} >github.token.compromised.secret.txt
 }
 function google_api_key () {
+    google_api1="AIzaSyDxJB-"
+    google_api2="2ocxX02LIAZiU_2_0bhhc2wDlN8g"
     echo "Commit: google_api_key"
     echo "${google_api1}${google_api2}"   >google_api_key.compromised.secret.txt
 }
 
 function azure_storage () {
+    azure_storage_1="e6ZZx75Z6095KHJvBZIDOD9kCLt3KjHx/"
+    azure_storage_2="KKT0LWi25F+Cq0XxlpS+tbs1EqlqvRY3YgC19T5+PC6+AStjkFjyQ=="
     echo "Commit: azure_storage"
     echo "${azure_storage_1}${azure_storage_2}"   >azure_storage.compromised.secret.txt
 }
 
 
-case $1 in 
+function npm_granular () {
+    npm_g1="npm_0QWV3DXVrcBZR"
+    npm_g2="srwh1ovdBWl2kjOtH0GzmRc"
+    echo "Commit: npm_granular"
+    echo "${npm_g1}${npm_g2}"   >npm_granular.compromised.secret.txt
+}
+
+
+case $keyname in 
  github)
     github_pat
     ;;
@@ -47,14 +59,18 @@ case $1 in
  azure_storage)
     azure_storage
     ;;
+ npm_granular)
+    npm_granular
+    ;;
  all)
     github_pat
     google_api_key 
     azure_storage
+    npm_granular 
     ;;
  *)
    echo 
-   echo "Please pass a name of token to compromise: [ azure_storage, github, google_api_key ]"
+   echo "Please pass a name of token to compromise: [ azure_storage, github, google_api_key, npm_granular ]"
    echo 
    ;;
 esac
