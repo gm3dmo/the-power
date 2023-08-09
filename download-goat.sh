727a7f0b420270691c5d468203a96bf69f4758bb
David Morris <gm3dmo@github.com>
2023-08-09 18:36:50 +0100

    Adding goat simulator.

---
+++ download-goat.sh
@@ -0,0 +1,32 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#download-a-repository-archive-zip
+# GET /repos/{owner}/{repo}/zipball/{ref}
+
+goat_org=WebGoat
+goat_repo=WebGoat
+
+goat_archive=WebGoat.archive.zip
+
+curl ${curl_custom_flags} \
+     -L \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/repos/${goat_org}/${goat_repo}/zipball/${base_branch}" -o "tmp/${repo}.archive.zip"
+
+
+cd src/${repo}
+pwd
+unzip ../../tmp/${goat_archive}
+cd ..
+mv ${repo}/WebGoat-WebGoat-4ba8185/* ${repo}
+rmdir ${repo}/WebGoat-WebGoat-4ba8185
+cd ${repo}
+
+git add *
+git commit -m "Adding the goat app."
+echo
+echo press enter to push commit with goat:
+echo
+read x
+git push
