4a4a3bf11d5859ca7d65f4cd9e86b206ac159ec8
David Morris <gm3dmo@github.com>
2023-08-09 10:43:19 +0100

    Adding npm classic.

--- test-commit-secret.sh
+++ test-commit-secret.sh
@@ -18,26 +18,27 @@ if [ -z "$1" ]
     keyname=$1
 fi
 
-
-
 function github_pat () { 
     gh1=ghp_9
     gh2=gDO8kvREKK9toy7CsUunZEY77XvGA1pNv5F
     echo "Commit: GitHub PAT"
     echo export GITHUB_TOKEN=${gh1}${gh2} >github.token.compromised.secret.txt
 }
+
+
 function google_api_key () {
     google_api1="AIzaSyDxJB-"
     google_api2="2ocxX02LIAZiU_2_0bhhc2wDlN8g"
     echo "Commit: google_api_key"
-    echo "${google_api1}${google_api2}"   >google_api_key.compromised.secret.txt
+    echo "${google_api1}${google_api2}" >google_api_key.compromised.secret.txt
 }
+
 
 function azure_storage () {
     azure_storage_1="e6ZZx75Z6095KHJvBZIDOD9kCLt3KjHx/"
     azure_storage_2="KKT0LWi25F+Cq0XxlpS+tbs1EqlqvRY3YgC19T5+PC6+AStjkFjyQ=="
     echo "Commit: azure_storage"
-    echo "${azure_storage_1}${azure_storage_2}"   >azure_storage.compromised.secret.txt
+    echo "${azure_storage_1}${azure_storage_2}" >azure_storage.compromised.secret.txt
 }
 
 
@@ -45,11 +46,19 @@ function npm_granular () {
     npm_g1="npm_0QWV3DXVrcBZR"
     npm_g2="srwh1ovdBWl2kjOtH0GzmRc"
     echo "Commit: npm_granular"
-    echo "${npm_g1}${npm_g2}"   >npm_granular.compromised.secret.txt
+    echo "${npm_g1}${npm_g2}" >npm_granular.compromised.secret.txt
 }
 
 
-case $keyname in 
+function npm_classic () {
+    npm_c1="npm_Fxg6NNBNSxFDTfAQ"
+    npm_c2="pWABbI87Bl6laH1Mk1dH"
+    echo "Commit: npm_classic"
+    echo "${npm_c1}${npm_c2}" >npm_classic.compromised.secret.txt
+}
+
+
+case ${keyname} in 
  github)
     github_pat
     ;;
@@ -61,6 +70,9 @@ case $keyname in
     ;;
  npm_granular)
     npm_granular
+    ;;
+ npm_classic)
+    npm_classic
     ;;
  all)
     github_pat
