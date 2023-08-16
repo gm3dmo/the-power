8a7e52535dc82e8a5efbf71e2bb1a04a79ed0924
David Morris <gm3dmo@github.com>
2023-08-16 13:07:37 +0100

    Updating list commits on repo.

--- list-commits-on-repo.sh
+++ list-commits-on-repo.sh
@@ -1,9 +1,14 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
-# GET /repos/:owner/:repo/commits
+# https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits
+# GET /repos/{owner}/{repo}/commits
+
+
+# ./list-commits-on-repo.sh  | jq -r '.[] | [ .sha, .commit.author.date, .commit.message ] | @csv
+
+sha=${1:base_branch}
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/commits
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/commits?sha=${sha}"
