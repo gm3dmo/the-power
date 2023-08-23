71993c62a432a32ef2381adaf7cafff2a2fc74cb
David Morris <gm3dmo@github.com>
2023-08-23 11:04:56 +0100

    Adding url for dispatch event.

--- dispatch-event.sh
+++ dispatch-event.sh
@@ -1,7 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/repos#create-a-repository-dispatch-event
-# POST /repos/:owner/:repo/dispatches
+# https://docs.github.com/en/enterprise-cloud@latest/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event
+# POST /repos/{owner}/{repo}/dispatches
 
 # This script will send a repository dispatch event. If you have configured
 # the simple workflow in the power then sending this event will trigger that
