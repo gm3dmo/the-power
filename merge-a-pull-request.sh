96e5cd22bd75992207268233f6cf2c4a44eec5d2
David Morris <gm3dmo@github.com>
2023-09-11 21:23:06 +0100

    Fixes #111

--- merge-a-pull-request.sh
+++ merge-a-pull-request.sh
@@ -12,8 +12,23 @@ if [ -z "$1" ]
     pull_number=$1
 fi
 
+commit_title="PR commit title"
+commit_message="PR commit message."
+
+json_file=tmp/merge-a-pull-request.json
+
+jq -n \
+           --arg commit_title "${commit_title}" \
+           --arg commit_message "${commit_message}" \
+           '{
+             commit_title : $commit_title,
+             commit_message : $commit_message,
+           }' > ${json_file}
+
+
 curl ${curl_custom_flags} \
      -X PUT \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/pulls/${pull_number}/merge
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/pulls/${pull_number}/merge" --data @${json_file}
+
