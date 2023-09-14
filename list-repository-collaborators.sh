4e9844ed8125ef988fdd001636f6342bb60e8811
David Morris <gm3dmo@github.com>
2023-09-14 11:36:12 +0100

    adding parameter to call.

--- list-repository-collaborators.sh
+++ list-repository-collaborators.sh
@@ -3,9 +3,12 @@
 # https://docs.github.com/en/rest/reference/collaborators#list-repository-collaborators
 # GET /repos/{owner}/{repo}/collaborators
 
+#affiliation="all"
+#affiliation="direct"
+affiliation="outside"
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/collaborators
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/collaborators?affiliation=${affiliation}"
 
