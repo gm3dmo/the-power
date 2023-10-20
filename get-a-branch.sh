a7f78fb65ef8ca783924c871bc7fa88dcf598edc
David Morris <gm3dmo@github.com>
2023-10-20 14:37:34 +0100

    Improve get-a-branch.

--- get-a-branch.sh
+++ get-a-branch.sh
@@ -3,9 +3,18 @@
 # https://docs.github.com/en/rest/reference/repos#get-a-branch
 # GET /repos/{owner}/{repo}/branches/{branch}
 
-branch=${base_branch}
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    branch=${base_branch}
+  else
+    branch=$1
+fi
+
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/branches/${branch}
+
+
