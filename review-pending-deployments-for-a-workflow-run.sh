ed41eddb649d3c5d0accb2d9f2088b6dfbabae34
David Morris <gm3dmo@github.com>
2023-10-26 17:20:55 +0100

    Adding review-pending-deployments-for-a-workflow-run.sh

--- review-pending-deployments-for-a-workflow-run.sh
+++ review-pending-deployments-for-a-workflow-run.sh
@@ -10,23 +10,19 @@ if [ -z "$1" ]
     repo=$1
 fi
 
-
 run_id=$(./list-workflow-runs-for-a-repository.sh | jq -r '.workflow_runs[-1].id')
-
 
 json_file=tmp/review-pending-deplyments-for-a-workflow-run.json
 
+environment_id=$(./get-an-environment.sh| jq -r '.id')
+
 jq -n \
-           --arg name "${repo}" \
-           '{
-             name : $name,
-           }' > ${json_file}
+           --argjson environment_id ${environment_id} \
+           '{ "environment_ids":[ $environment_id ], "state":"approved","comment":"Ship it!" }' > ${json_file}
 
-
-set -x
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/runs/${run_id}/pending_deployments"
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/runs/${run_id}/pending_deployments" -d @${json_file}
 
