6e4a29d139318cc6be2c504389fa55474ed35390
David Morris <gm3dmo@github.com>
2023-11-20 11:13:47 +0000

    updating add owner script.

--- add-owner-to-many-organizations.sh
+++ add-owner-to-many-organizations.sh
@@ -1,9 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/orgs#set-organization-membership-for-a-user
-# PUT /orgs/:org/memberships/:username
-
-user="bart"
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/members?apiVersion=2022-11-28#set-organization-membership-for-a-user
+# PUT /orgs/{org}/memberships/{username}
 
 for org in $(cat tmp/longlistoforgs.txt)
 do
@@ -11,6 +9,6 @@ do
        -X PUT \
        -H "Accept: application/vnd.github.v3+json" \
        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-          ${GITHUB_API_BASE_URL}/orgs/${org}/memberships/${user} --data '{"role": "admin"}'
+          "${GITHUB_API_BASE_URL}/orgs/${org}/memberships/${user}" --data '{"role": "admin"}'
 
 done
