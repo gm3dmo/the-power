c1af0427821eccf8c8260c8d4a2bc03d327c734d
David Morris <gm3dmo@github.com>
2023-11-23 20:25:12 +0000

    Adding stdout/stderr

--- powerflask.py
+++ powerflask.py
@@ -1,10 +1,17 @@
 import os
+import json
+import subprocess
+
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import SelectField
 
 from pygments import highlight
+from pygments.lexers import HttpLexer
 from pygments.lexers import JsonLexer
+from pygments.lexers import get_lexer_by_name
+
+from pygments.formatters import HtmlFormatter
 from pygments.formatters import HtmlFormatter
 
 app = Flask(__name__)
@@ -34,15 +41,29 @@ def index():
 
     # Initialize the output variable
     output = None
+    colorized_output = None
+    headers = None
+    colorized_error = None
 
     if request.method == 'POST' and form.validate_on_submit():
         # Get the selected script filename from the form
         script_filename = form.script.data
-        # Execute the script and capture the output
-        output = os.popen(f"sh {SCRIPTS_DIR}/{script_filename}").read()
-        colorized_output = highlight(output, JsonLexer(), HtmlFormatter())
+        process = subprocess.Popen(["sh", f"{SCRIPTS_DIR}/{script_filename}"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output1, error = process.communicate()
+        output1 = output1.decode('utf-8')  # decode from bytes to string
+        error = error.decode('utf-8')  # decode from bytes to string
+        print(f"""output1: {output1}\n""")
 
-    return render_template('index.html', form=form, output=output, colorized_output=colorized_output)
+        print(f"""error: {error}\n""")
+
+        #output = os.popen(f"sh {SCRIPTS_DIR}/{script_filename}").read()
+        output = output1
+        colorized_output = highlight(output, JsonLexer(), HtmlFormatter())
+        lexer = get_lexer_by_name("http", stripall=True)
+        colorized_error = highlight(error, lexer, HtmlFormatter())
+        
+
+    return render_template('index.html', form=form, colorized_output=colorized_output, colorized_error=colorized_error)
 
 if __name__ == '__main__':
     app.run(debug=True)
