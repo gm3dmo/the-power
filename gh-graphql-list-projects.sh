9861dc8c7cb40264b418f45339e44789c4302f92
David Morris <gm3dmo@github.com>
2023-11-29 21:16:27 +0000

    Adding a graphql test

---
+++ gh-graphql-list-projects.sh
@@ -0,0 +1,21 @@
+.  ./.gh-api-examples.conf
+
+# https://cli.github.com/manual/gh_api
+
+export GH_HOST=${hostname}
+export GH_TOKEN=${GITHUB_TOKEN}
+export GH_ENTERPRISE_TOKEN=${GITHUB_TOKEN}
+
+gh api graphql --paginate -F owner="${org}"  -f query='
+query($owner: String!) {
+    organization(login: $owner) {
+      projectsV2(first: 10) {
+        nodes {
+          id
+          title
+        }
+      }
+    }
+  }'
+
+
