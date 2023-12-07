d386e8f7b76be0682d50f2b98671661f4ea8ddea
David Morris <gm3dmo@github.com>
2023-12-07 14:00:31 +0000

    Adding copilot example fixing org to

--- get-copilot-for-business-seat-information-and-settings-for-an-organization.sh
+++ get-copilot-for-business-seat-information-and-settings-for-an-organization.sh
@@ -8,4 +8,4 @@ curl ${curl_custom_flags} \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/orgs/{org}/copilot/billing"
+        "${GITHUB_API_BASE_URL}/orgs/${org}/copilot/billing"
