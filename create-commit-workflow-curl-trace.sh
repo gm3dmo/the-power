5e092282ef4e721ef57ff83e4ba95d1c6c3f017c
David Morris <gm3dmo@github.com>
2024-01-11 17:53:04 +0000

    Adding workflow curl trace

---
+++ create-commit-workflow-curl-trace.sh
@@ -0,0 +1,18 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+# PUT /repos/:owner/:repo/contents/:path
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+curl ${curl_custom_flags} \
+     -X PUT \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/.github/workflows/workflow-curl-trace.yml --data @tmp/workflow-curl-trace.json
