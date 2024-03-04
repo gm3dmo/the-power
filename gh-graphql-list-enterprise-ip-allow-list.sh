e6107da9180232369e84fe4d2191fa897b7bf603
David Morris <gm3dmo@github.com>
2024-03-04 16:12:55 +0000

    Adding enterprise ip allow list

---
+++ gh-graphql-list-enterprise-ip-allow-list.sh
@@ -0,0 +1,29 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/graphql/reference/objects#ipallowlistentry
+
+enterprise=${1:-$enterprise}
+
+
+export GH_TOKEN=${GITHUB_TOKEN}
+
+gh api graphql -f enterprise="${enterprise}" -F query='
+query($enterprise: String! $endCursor: String)  {
+ enterprise(slug: $enterprise ) {
+                    ownerInfo {
+                        ipAllowListEntries(first: 100, after: $endCursor) {
+                            nodes {
+                                id
+                                allowListValue
+                                createdAt
+                                isActive
+                                name
+                            }
+                            pageInfo {
+                                endCursor
+                                hasNextPage
+                            }
+                        }
+                    }
+                }
+}'
