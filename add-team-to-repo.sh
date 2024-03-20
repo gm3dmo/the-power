bb4058b8a866d2bbdb0274537b9a7243c0030457
David Morris <gm3dmo@github.com>
2024-03-20 09:11:11 +0000

    Adding newer urls for docs

--- add-team-to-repo.sh
+++ add-team-to-repo.sh
@@ -1,7 +1,7 @@
 .   ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/teams#add-or-update-team-repository-permissions
-# PUT /orgs/:org/teams/:team_slug/repos/:owner/:repo
+# https://docs.github.com/en/enterprise-cloud@latest/rest/teams/teams?apiVersion=2022-11-28#add-or-update-team-repository-permissions
+# PUT /orgs/{org}/teams/{team_slug}/repos/{owner}/{repo}
 
 if [ -z "$1" ]
   then
@@ -24,4 +24,4 @@ curl ${curl_custom_flags} \
      -X PUT \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/teams/${team_id}/repos/${org}/${repo} --data @${json_file}
+        "${GITHUB_API_BASE_URL}/teams/${team_id}/repos/${org}/${repo}" --data @${json_file}
