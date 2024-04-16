fed5460d0d34e96cd04818e84dd609079b6d7785
David Morris <gm3dmo@github.com>
2024-04-16 10:53:28 +0100

    Adding github app version for oauth web flow

---
+++ tiny-github-app-oauth-web-application-flow-step1.sh
@@ -0,0 +1,19 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#web-application-flow
+# GET https://github.com/login/oauth/authorize
+
+# Step 1
+# https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#1-request-a-users-github-identity
+# GET https://github.com/login/oauth/authorize
+
+# This is a gnarly thing to do but saves rewriting how the config file
+# gets populated for this one script that uses github.com for the device flow.
+#
+if [ $hostname == "api.github.com" ];
+then
+  hostname="github.com"
+fi
+
+
+open -n -a "Google Chrome" --args --profile-directory="${chrome_profile}"  "http://${hostname}/login/oauth/authorize?client_id=${client_id}"
---
+++ tiny-github-app-oauth-web-application-flow-step2.sh
@@ -0,0 +1,24 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#web-application-flow
+# GET https://github.com/login/oauth/authorize
+
+
+# # This is a gnarly thing to do but saves rewriting how the config file
+# gets populated for this one script that uses github.com for the device flow.
+if [ $hostname == "api.github.com" ];
+then
+  hostname="github.com"
+fi
+
+# Step 2.
+# "code" is the code gathered in the step 1 script. It should have opened in a chrome browser
+# with a url like: https://example.com/callback?code=7fb98c2417446ce64c2d
+# you need to pass "7fb98c2417446ce64c2d" to this script.
+
+code=$1
+
+curl ${curl_custom_flags} -L \
+     -H "Accept: application/vnd.github.v3+json" \
+     -X POST \
+        "http://${hostname}/login/oauth/access_token?client_id=${client_id}&client_secret=${app_client_secret}&code=${code}" | jq -r
