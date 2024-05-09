fce3277ca013ab676789ce4dca52d2900f37840f
David Morris <gm3dmo@github.com>
2024-05-09 10:50:56 +0100

    Fix doc link and DELETE header

--- cancel-an-organization-invitation.sh
+++ cancel-an-organization-invitation.sh
@@ -1,7 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/orgs#create-an-organization-invitation
-# POST /orgs/{org}/invitations
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/members?apiVersion=2022-11-28#cancel-an-organization-invitation
+# DELETE /orgs/{org}/invitations/{invitation_id}
 
 invitation_id=$1
 
@@ -9,4 +9,5 @@ curl ${curl_custom_flags} \
      -X DELETE \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/orgs/${org}/invitations/${invitation_id}
+        "${GITHUB_API_BASE_URL}/orgs/${org}/invitations/${invitation_id}"
+
