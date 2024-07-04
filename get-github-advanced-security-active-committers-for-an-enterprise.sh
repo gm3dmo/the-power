84a1f64ed10c71ed391ac06b361911d7f425af39
David Morris <gm3dmo@github.com>
2024-07-04 20:24:57 +0100

    Addin advanced security committers

---
+++ get-github-advanced-security-active-committers-for-an-enterprise.sh
@@ -0,0 +1,11 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/enterprise-admin/billing?apiVersion=2022-11-28#get-github-advanced-security-active-committers-for-an-enterprise
+# GET /enterprises/{enterprise}/settings/billing/advanced-security
+
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/settings/billing/advanced-security"  --data @${json_file}
