9d25c7170a6135d06bdd0b1c815288d5637a4392
David Morris <gm3dmo@github.com>
2024-07-11 11:44:40 +0100

    Adding this dependencies script

--- get-a-diff-of-the-dependencies-between-commits.sh
+++ get-a-diff-of-the-dependencies-between-commits.sh
@@ -1,35 +1,24 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/dependency-graph/dependency-review#get-a-diff-of-the-dependencies-between-commits
+# https://docs.github.com/en/rest/dependency-graph/dependency-review?apiVersion=2022-11-28#get-a-diff-of-the-dependencies-between-commits
 # GET /repos/{owner}/{repo}/dependency-graph/compare/{basehead}
 
-# if argument $1 is passed use that as variable head
 
+# If the script is passed an argument $1 use that as the name
 if [ -z "$1" ]
-then
-  head=$(./list-commits-on-repo.sh | jq -r '.[0].sha')
-else
-  head="$1"
+  then
+    repo=$repo
+  else
+    repo=$1
 fi
 
-# if argument $2 is passed use that as a variable base
 
-if [ -z "$2" ]
-then
-  base="$(./list-commits-on-repo.sh | jq -r '.[-1].sha')"
-else
-  base="$2"
-fi
+#basehead="2156e9ba0f3ec66544189b1b15fa8e3474dcd0fe...ffaf13e998cc376175965f0549fdd2abd324a6e0"
+basehead="ffaf13e998cc376175965f0549fdd2abd324a6e0...7d069871c2c39850aa3a4e1be7acbb8aa8f7103f"
 
-# format the basehead variable
-
-basehead=(${base}...${head})
-
-# send a request to GitHub API
-
-set -x
-curl -v ${curl_custom_flags} \
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-     ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/dependency-graph/compare/${basehead}
+        "${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/dependency-graph/compare/${basehead}"
 
