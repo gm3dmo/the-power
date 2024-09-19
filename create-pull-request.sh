f7d2537b67b5bcec5660ac03c602ebffa720a487
David Morris <gm3dmo@github.com>
2024-09-19 16:34:06 +0100

    Adding draft capability

--- create-pull-request.sh
+++ create-pull-request.sh
@@ -1,7 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
-# POST /repos/:owner/:repo/pulls
+# https://docs.github.com/en/enterprise-cloud@latest/rest/pulls/pulls?apiVersion=2022-11-28#create-a-pull-request
+# POST /repos/{owner}/{repo}/pulls
 
 json_file=tmp/create-pull-request.json
 rm -f ${json_file}
@@ -11,17 +11,20 @@ title="Amazing new feature PR: $timestamp"
 
 lorem_file=test-data/lorem-pull-request.md
 lorem_text=$(cat ${lorem_file})
+draft_pull_request="true"
 
 jq -n \
   --arg title "${title}" \
   --arg body "${lorem_text} see research by: @${default_committer}" \
   --arg head "${branch_name}" \
   --arg base "${base_branch}" \
+  --arg draft "${draft_pull_request}" \
   '{
     title: $title,
     body: $body,
     head: $head,
-    base: $base
+    base: $base,
+    draft: $draft | test("true") 
   }' > ${json_file}
 
 curl ${curl_custom_flags} \
