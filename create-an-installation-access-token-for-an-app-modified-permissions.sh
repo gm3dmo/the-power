5b8928878710cb527dbb98843f4e852c39f6eec9
David Morris <gm3dmo@github.com>
2024-10-16 17:45:57 +0100

    Adding configurable json for create-an-installation-access-token

--- create-an-installation-access-token-for-an-app-modified-permissions.sh
+++ create-an-installation-access-token-for-an-app-modified-permissions.sh
@@ -7,12 +7,29 @@
 # actually has. For this case, create a GitHub app with write on contents and
 # write on issues. The token created will have read only privileges on contents and issues.
 
+
 installation_id=${default_installation_id}
 
 JWT=$(./tiny-call-get-jwt.sh ${default_app_id})
 
-curl ${curl_custom_flags} \
-     -H "Authorization: Bearer ${JWT}" \
-        "${GITHUB_API_BASE_URL}/app/installations/${installation_id}/access_tokens" --data '{"permissions": {"contents":"write" } }'
+json_file=tmp/create-an-installation-access-token-for-an-app.json
 
+# Add any permissions you want here (must be valid json):
+permissions='{"contents": "write", "issues": "write"}'
+
+jq -n \
+       --argjson permissions "${permissions}" \
+           '{
+             permissions :  $permissions 
+           }' > ${json_file}
+
+echo ===================== >&2
+cat ${json_file} | jq -r >&2
+echo ===================== >&2
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${JWT}" \
+        "${GITHUB_API_BASE_URL}/app/installations/${installation_id}/access_tokens" --data @${json_file}
 
