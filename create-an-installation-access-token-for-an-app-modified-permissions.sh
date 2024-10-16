0610c63f1a22118e75fd77429602acbe388c2b2d
David Morris <gm3dmo@github.com>
2024-10-16 17:02:55 +0100

    Fixing up app modified permissions script

--- create-an-installation-access-token-for-an-app-modified-permissions.sh
+++ create-an-installation-access-token-for-an-app-modified-permissions.sh
@@ -12,7 +12,7 @@ installation_id=${default_installation_id}
 JWT=$(./tiny-call-get-jwt.sh ${default_app_id})
 
 curl ${curl_custom_flags} \
-     -X POST \
      -H "Authorization: Bearer ${JWT}" \
-     -H "Accept: application/vnd.github.machine-man-preview+json" \
-        ${GITHUB_API_BASE_URL}/app/installations/${installation_id}/access_tokens --data '{"permissions": {"contents":"read", "issues": "read"} }'
+        "${GITHUB_API_BASE_URL}/app/installations/${installation_id}/access_tokens" --data '{"permissions": {"contents":"write" } }'
+
+
