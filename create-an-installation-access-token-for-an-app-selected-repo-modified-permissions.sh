a54e37cdd96d952fdf82434d02a48d0c2795413b
David Morris <gm3dmo@github.com>
2024-10-17 19:05:57 +0100

    Adding installation access token for an app script

---
+++ create-an-installation-access-token-for-an-app-selected-repo-modified-permissions.sh
@@ -0,0 +1,39 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/apps#create-an-installation-access-token-for-an-app
+# POST /app/installations/{installation_id}/access_tokens
+
+# This script gets an access token which has less privilege than the GitHub App
+# actually has. For this case, create a GitHub app with write on contents and
+# write on issues. The token created will have read only privileges on contents and issues.
+
+
+installation_id=${default_installation_id}
+
+JWT=$(./tiny-call-get-jwt.sh ${default_app_id})
+
+json_file=tmp/create-an-installation-access-token-for-an-app.json
+
+# Add any permissions you want here (must be valid json):
+permissions='{"contents": "write", "issues": "write"}'
+
+repositories='["testrepo"]'
+
+jq -n \
+       --argjson permissions "${permissions}" \
+       --argjson repositories "${repositories}" \
+           '{
+             permissions :  $permissions,
+             repositories :  $repositories
+           }' > ${json_file}
+
+echo ===================== >&2
+cat ${json_file} | jq -r >&2
+echo ===================== >&2
+
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${JWT}" \
+        "${GITHUB_API_BASE_URL}/app/installations/${installation_id}/access_tokens" --data @${json_file}
+
