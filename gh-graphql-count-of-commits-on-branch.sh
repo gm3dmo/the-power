5dad59e50a973c6047b66278358f07f30ec76c2e
David Morris <gm3dmo@github.com>
2024-10-21 19:20:34 +0100

    Make branch configurable/passable

--- gh-graphql-count-of-commits-on-branch.sh
+++ gh-graphql-count-of-commits-on-branch.sh
@@ -4,7 +4,14 @@
 
 export GH_TOKEN=${GITHUB_TOKEN}
 
-branch=new_branch
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    branch=$branch_name
+  else
+    branch=$1
+fi
 
 gh api graphql --paginate -F owner="${owner}" -f repo="${repo}" -F branch="${branch}" -f query='
 query($owner: String!,  $repo: String!, $branch: String! ) {
