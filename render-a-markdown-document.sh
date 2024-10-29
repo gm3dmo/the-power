6759946c63e177984a3158edfb3470e1b0981a3a
David Morris <gm3dmo@github.com>
2024-10-29 15:08:24 +0000

    Adding more render markdown features

--- render-a-markdown-document.sh
+++ render-a-markdown-document.sh
@@ -4,17 +4,38 @@
 # POST /markdown
 
 
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    mode=markdown
+  else
+    mode=$1
+fi
+
+
+echo "mode: ${mode}" >&2
+
+context="${owner}/${repo}"
+
+
 json_file=tmp/render-a-markdown-document.json
 
 jq -n \
-           --arg text "hello **world**" \
+           --arg text "## Title\nhello **world** In the issue #1 and the PR #2" \
+           --arg mode "$mode" \
+           --arg context "$context" \
            '{
              text: $text,
-           }' > ${json_file}
+             mode: $mode,
+             context: $context,
+            }' > ${json_file}
+
+
+cat $json_file | jq -r
 
 
 curl ${curl_custom_flags} \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/markdown"  --data @${json_file}
+        "${GITHUB_API_BASE_URL}/markdown?mode=${mode}"  --data @${json_file}
