62f333303c0957e07445d67772e7af9ad385d8b8
David Morris <gm3dmo@github.com>
2024-11-13 16:16:18 +0000

    Adding users

--- generate-all-users-csv.sh
+++ generate-all-users-csv.sh
@@ -1,8 +1,6 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/enterprise-server/admin/configuration/configuring-your-enterprise/site-admin-dashboard
+# https://docs.github.com/en/enterprise-server@3.14/admin/monitoring-activity-in-your-enterprise/exploring-user-activity-in-your-enterprise/accessing-reports-for-your-instance#downloading-reports-programmatically
 
 curl -v -u ${admin_user}:${GITHUB_TOKEN} \
-        https://${hostname}/stafftools/reports/all_users.csv  -o tmp/all_users.csv
-
-
+        "https://${hostname}/stafftools/reports/all_users.csv"  -o tmp/all_users.csv
