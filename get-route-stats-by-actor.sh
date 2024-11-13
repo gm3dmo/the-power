9116250686717218fae6fbc874be1de128f8ac24
David Morris <gm3dmo@github.com>
2024-11-13 11:35:16 +0000

    Adding route by stats for #176

---
+++ get-route-stats-by-actor.sh
@@ -0,0 +1,19 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/api-insights?apiVersion=2022-11-28#get-route-stats-by-actor
+# GET /orgs/{org}/insights/api/route-stats/{actor_type}/{actor_id}
+
+actor_type="installations"
+actor_id=${default_installation_id}
+
+TS=$(date +%s)
+MIN_TIMESTAMP=$((${TS}-86400))
+MAX_TIMESTAMP=$((${TS}-1000))
+
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/orgs/${org}/insights/api/route-stats/${actor_type}/${actor_id}?min_timestamp=${MIN_TIMESTAMP}&max_timestamp=${MAX_TIMESTAMP}"
+
