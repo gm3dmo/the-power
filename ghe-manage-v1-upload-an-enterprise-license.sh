4e01c12fe7a3ea2811d4b33cca361349797022d4
David Morris <gm3dmo@github.com>
2024-11-15 19:27:53 +0000

    Adding ghe-manage-v1-upload-an-enterprise-license.sh for #178

---
+++ ghe-manage-v1-upload-an-enterprise-license.sh
@@ -0,0 +1,23 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-server@3.14/rest/enterprise-admin/manage-ghes?apiVersion=2022-11-28#upload-an-enterprise-license
+# PUT /manage/v1/config/license
+
+# Note the documentation says: Uploads an enterprise license. This operation does not automatically activate the license.
+
+
+if [ -z "$1" ]
+  then
+    license_file="tmp/enterprise.ghl"
+  else
+    license_file=$1
+fi
+
+
+apply=true
+
+curl -L ${curl_custom_flags} \
+        -X PUT \
+        -u "api_key:${mgmt_password}" \
+        -H "Content-Type: multipart/form-data" \
+           "https://${hostname}:${mgmt_port}/manage/v1/config/license?apply=${apply}" --form "license=@${license_file}"  
