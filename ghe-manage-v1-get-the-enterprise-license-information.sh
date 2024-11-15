dd53d5126b6cdf51d1f3daddce7ee228382ec330
David Morris <gm3dmo@github.com>
2024-11-15 17:17:36 +0000

    Adding ghe-manage-v1-get-the-enterprise-license-information.sh for #178

---
+++ ghe-manage-v1-get-the-enterprise-license-information.sh
@@ -0,0 +1,10 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-server@3.14/rest/enterprise-admin/manage-ghes?apiVersion=2022-11-28#get-the-enterprise-license-information
+# GET /manage/v1/config/license
+
+
+curl -L ${curl_custom_flags} \
+        -u "api_key:${mgmt_password}" \
+        "https://${hostname}:${mgmt_port}/manage/v1/config/license"
+
