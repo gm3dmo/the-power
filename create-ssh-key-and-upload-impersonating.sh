348c4d2537449b99e6acdcdd8be71722163295fc
David Morris <gm3dmo@github.com>
2024-11-16 17:49:06 +0000

    resolve key problems 183 (#185)
    * Fix the paths because I am bad and used the shell for this #183
    
    * Adding pyinfra script for creating users #183

--- create-ssh-key-and-upload-impersonating.sh
+++ create-ssh-key-and-upload-impersonating.sh
@@ -5,28 +5,30 @@
 
 user_to_impersonate=${1:-$default_committer}
 
+echo impersonating $user_to_impersonate
+
 IMPERSONATION_TOKEN=$(bash create-impersonation-oauth-token.sh ${user_to_impersonate}| jq -r '.token')
 GITHUB_TOKEN=${IMPERSONATION_TOKEN}
 
 json_file="tmp/create-ssh-key-and-upload-impersonating.json"
 
-ssh_key_file=tmp/ed25519_${team_member}
-ssh_public_key_file=tmp/ed25519_${team_member}.pub
+ssh_key_file=tmp/ed25519_${user_to_impersonate}
+ssh_public_key_file=tmp/ed25519_${user_to_impersonate}.pub
 
 rm -f ${ssh_key_file} ${ssh_public_key_file}
 
-ssh-keygen -t ed25519 -f ${ssh_key_file} -q -N '""' -C "${user_to_impersonate}@example.com"
+ssh-keygen -t ed25519 -f ${ssh_key_file} -q -N '' -C "${user_to_impersonate}@example.com"
 
+ssh-keygen -l -v -f ${ssh_public_key_file}
 
-public_key_file=${ssh_key_file}
 public_key=$(cat ${ssh_public_key_file})
 
 json_file=tmp/ssh-key-${user_to_impersonate}.json
 
-ts=$(date +%s)
+timestamp=$(date +%s)
 
 DATA=$(jq -n \
-                --arg title "The Power ssh key uploaded for ${user_to_impersonate} at timestamp: ${ts}" \
+                --arg title "The Power ssh key uploaded for ${user_to_impersonate} at timestamp: ${timestamp}" \
                 --arg key "${public_key}" \
                 '{title: $title, key: $key}' )
 echo $DATA > ${json_file}
