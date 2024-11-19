717063e75f29a1686ccf20ef10dc31a94dd8fce9
David Morris <gm3dmo@github.com>
2024-11-19 15:25:20 +0000

    Update hooky.py

--- hooky.py
+++ hooky.py
@@ -15,6 +15,7 @@ import sys
 import json
 import string
 import time
+import logzero
 from flask import Flask
 from flask import request
 
@@ -24,13 +25,21 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def slurphook():
     if request.method == 'POST':
-        print("hook triggered")
-        print(request.json)
-        print("sleeping")
-        time.sleep(2)
+        if request.headers.getlist("X-Forwarded-For"):
+            ip = request.headers.getlist("X-Forwarded-For")[0]
+        else:
+            ip = request.remote_addr
+        data = request.json
+        keys = list(data.keys())
+        first_key = keys[0]
+        hook_action = data[first_key]
+        hook_target = keys[1]
+        print(f"""hook received for target = "{hook_target}" action = "{hook_action}" triggered by ip address: (should be part of https://api.github.com/meta): {ip}""")
+        print(f"-----")
+        #print(f"{request.json}")
         return ('status',200)
 
 
 if __name__ == '__main__':
-     app.config['DEBUG'] = True
-     app.run(host='0.0.0.0', port=8080)
+     app.config['DEBUG'] = False
+     app.run(host='localhost', port=8000)
