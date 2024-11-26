ad76d4e42268e7fb9ca48ef06b5cf69277ef5bea
David Morris <gm3dmo@github.com>
2024-11-26 10:41:07 +0000

    Adding graphql script (#189)

---
+++ gh-graphql-repo-users-with-permission-sources.sh
@@ -0,0 +1,39 @@
+.  ./.gh-api-examples.conf
+
+# https://cli.github.com/manual/gh_api
+# Demonstrates pagination for gh api
+
+export GH_TOKEN=${GITHUB_TOKEN}
+
+gh api graphql -F owner="${org}" -F repo="${repo}" -f query='
+query($owner: String!, $repo: String!) {
+  repository(owner: $owner, name: $repo) {
+    nameWithOwner
+    collaborators(first: 100) {
+      edges {
+        permission
+        node {
+          login
+        }
+        permissionSources {
+          sourcePermission: permission
+          source {
+            ... on Team {
+              permissionSource: __typename
+              teamName: name
+            }
+            ... on Organization {
+              permissionSource: __typename
+              orgName: name
+            }
+            ... on Repository {
+              permissionSource: __typename
+              repoName: name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+'
