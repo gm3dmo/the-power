5e49d5c10a32913d30e1053957823db348266927
David Morris <gm3dmo@github.com>
2024-12-04 08:13:13 +0000

    add repository_selection (#200)
    * Adding org installations csv script
    
    * Adding list-app-installations-for-an-organization-csv.sh
    
    * Adding repository_selection

--- list-app-installations-for-an-organization-csv.sh
+++ list-app-installations-for-an-organization-csv.sh
@@ -9,7 +9,7 @@ curl ${curl_custom_flags} \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "${GITHUB_API_BASE_URL}/orgs/${org}/installations" > tmp/list-app-installations-for-an-organization.json
 
-jq -r '(["counter","id","app_id","app_slug","created_at","updated_at","suspended_by_login","suspended_at", "html_url"] | @csv), 
+jq -r '(["counter","id","app_id","app_slug", "repository_selection", "created_at","updated_at", "suspended_by_login","suspended_at", "html_url" ] | @csv), 
 (.installations | to_entries[] | 
-    [(.key + 1 | tostring), (.value.id | tostring), (.value.app_id | tostring), .value.app_slug, .value.created_at, .value.updated_at, 
-    (.value.suspended_by.login // null), (.value.suspended_at // null), .value.html_url ] | @csv)' tmp/list-app-installations-for-an-organization.json
+    [(.key + 1 | tostring), (.value.id | tostring), (.value.app_id | tostring), .value.app_slug, .value.repository_selection, .value.created_at, .value.updated_at, 
+
