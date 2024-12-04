86cf79f55f604cbb4c50ed2fa173deebef0960c5
David Morris <gm3dmo@github.com>
2024-12-04 08:33:40 +0000

    Adding list organization app installations

--- list-app-installations-for-an-organization-csv.sh
+++ list-app-installations-for-an-organization-csv.sh
@@ -1,7 +1,15 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/orgs#list-app-installations-for-an-organization
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/orgs?apiVersion=2022-11-28#list-app-installations-for-an-organization
 # GET /orgs/{org}/installations
+
+
+if [ -z "$1" ]
+  then
+    org=$org
+  else
+    org=$1
+fi
 
 
 curl ${curl_custom_flags} \
@@ -12,4 +20,4 @@ curl ${curl_custom_flags} \
 jq -r '(["counter","id","app_id","app_slug", "repository_selection", "created_at","updated_at", "suspended_by_login","suspended_at", "html_url" ] | @csv), 
 (.installations | to_entries[] | 
     [(.key + 1 | tostring), (.value.id | tostring), (.value.app_id | tostring), .value.app_slug, .value.repository_selection, .value.created_at, .value.updated_at, 
-
+    (.value.suspended_by.login // null), (.value.suspended_at // null), .value.html_url ] | @csv)' tmp/list-app-installations-for-an-organization.json
