acf36033ed4f51cd22ba335007a1cb648ba474af
David Morris <gm3dmo@github.com>
2024-12-04 08:38:34 +0000

    Adding list organization app installations (#201)

--- list-app-installations-for-an-organization.sh
+++ list-app-installations-for-an-organization.sh
@@ -1,7 +1,15 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/orgs/orgs#list-app-installations-for-an-organization
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/orgs?apiVersion=2022-11-28#list-app-installations-for-an-organization
 # GET /orgs/{org}/installations
+
+
+if [ -z "$1" ]
+  then
+    org=$org
+  else
+    org=$1
+fi
 
 
 curl ${curl_custom_flags} \
