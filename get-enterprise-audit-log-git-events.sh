0c031a56fafd4099578edc6352792c6f3d85beca
David Morris <gm3dmo@github.com>
2024-12-04 10:36:12 +0000

    Add a git events audit log enterprise script" (#202)
    * Adding a git events script
    
    * Adding get enterprise audit log split from git events

--- get-enterprise-audit-log-git-events.sh
+++ get-enterprise-audit-log-git-events.sh
@@ -3,7 +3,17 @@
 # https://docs.github.com/en/rest/enterprise-admin/audit-log
 # GET /enterprises/{enterprise}/audit-log
 
+
+if [ -z "$1" ]
+  then
+    enterprise=$enterprise
+  else
+    enterprise=$1
+fi
+
+
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-     "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/audit-log" 
+        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/audit-log?include=git" 
+
