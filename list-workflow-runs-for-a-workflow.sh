f32312302feaf9942a3af6065b74c2632d94934f
David Morris <gm3dmo@github.com>
2025-01-10 17:10:53 +0000

    Adding workflow list

--- list-workflow-runs-for-a-workflow.sh
+++ list-workflow-runs-for-a-workflow.sh
@@ -1,21 +1,18 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-workflow
-# GET /repos/:owner/:repo/actions/workflows/:workflow_id/runs
+# https://docs.github.com/en/enterprise-cloud@latest/rest/actions/workflow-runs?apiVersion=2022-11-28#list-workflow-runs-for-a-workflow
+# GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs
 
 
 if [ -z "$1" ]
   then
-    
     workflow_id=$(./list-repository-workflows.sh | jq '[.workflows[].id] | max')
   else
     workflow_id=$1
 fi
 
 
-
-
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/workflows/${workflow_id}/runs
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/workflows/${workflow_id}/runs"
