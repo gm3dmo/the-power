ad4892d1586677b4a300d9716021db876fc6396e
David Morris <gm3dmo@github.com>
2025-01-21 22:57:10 +0000

    Fix exception

--- hooky-secret-validation.py
+++ hooky-secret-validation.py
@@ -37,11 +37,11 @@ def verify_signature(payload_body, secret_token, signature_header):
         signature_header: header received from GitHub (x-hub-signature-256)
     """
     if not signature_header:
-        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+        raise HTTPException(description="x-hub-signature-256 header is missing!", response=None, code=403)
     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
     if not hmac.compare_digest(expected_signature, signature_header):
-        raise HTTPException(status_code=403, detail="Request signature didn't match signature on record")
+        raise HTTPException(description="Request signature didn't match signature on record", response=None, code=403)
     else:
         app.logger.debug("-" * 21)
         app.logger.debug("the webhook signature matches")
