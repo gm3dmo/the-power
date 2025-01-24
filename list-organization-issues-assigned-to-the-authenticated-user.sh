ec7b5bd2b6a76c3b23aacc9c6a3c54d3f1455fe4
David Morris <gm3dmo@github.com>
2025-01-24 11:12:18 +0000

    Adding lost organization issues script for #253

---
+++ list-organization-issues-assigned-to-the-authenticated-user.sh
@@ -0,0 +1,21 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/issues/issues?apiVersion=2022-11-28#list-organization-issues-assigned-to-the-authenticated-user
+# GET /orgs/{org}/issues
+
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    org=$org
+  else
+    org=$1
+fi
+
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/orgs/${org}/issues" 
+
