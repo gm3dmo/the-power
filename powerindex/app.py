c1655494a1a266bcdad7ff91afa5d5cc1310e8e2
David Morris <gm3dmo@github.com>
2025-01-29 21:03:24 +0000

    Move the config to the bottom

--- powerindex/app.py
+++ powerindex/app.py
@@ -104,4 +104,4 @@ def index():
                          config=config)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=8000) 
