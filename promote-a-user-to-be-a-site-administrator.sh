c8c48cae776b79a3117217f1f09d79d291207b7a
David Morris <gm3dmo@github.com>
2025-01-29 10:56:37 +0000

    Resolves #258

--- promote-a-user-to-be-a-site-administrator.sh
+++ promote-a-user-to-be-a-site-administrator.sh
@@ -5,14 +5,15 @@
 
 if [ -z "$1" ]
   then
-    username=mona
+    username=${org_owner}
   else
     username=$1
 fi
 
 curl ${curl_custom_flags} \
+     -X PUT \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/users/${username}/site_admin "
+        "${GITHUB_API_BASE_URL}/users/${username}/site_admin"
 
