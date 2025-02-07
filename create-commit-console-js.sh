4339eb48c5fe47f393abab3ede8ceadf3b264eb7
David Morris <gm3dmo@github.com>
2025-02-07 11:46:38 +0000

    Updates for #277 automatically add the console.js file

---
+++ create-commit-console-js.sh
@@ -0,0 +1,22 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+# PUT /repos/:owner/:repo/contents/:path
+
+if [ -z "$1" ]
+  then
+    repo=$repo
+  else
+    repo=$1
+fi
+
+python3 create-commit-console-js.py
+filename_in_repo="console.js"
+json_file=tmp/create-commit-console-js.json
+
+curl ${curl_custom_flags} \
+     -X PUT \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+     -H "Accept: application/vnd.github.v3+json" \
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/contents/${filename_in_repo}" --data @${json_file}
+
