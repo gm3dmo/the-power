df483f84212b6377cb246fd41c6d1ca4a4ebf050
David Morris <gm3dmo@github.com>
2025-02-11 22:47:54 +0000

    Fixing create repo secret codespace

--- create-or-update-a-repository-codespaces-secret.sh
+++ create-or-update-a-repository-codespaces-secret.sh
@@ -1,12 +1,23 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/actions#create-or-update-a-repository-secret
+# https://docs.github.com/en/enterprise-cloud@latest/rest/codespaces/repository-secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
 # PUT /repos/{owner}/{repo}/codespaces/secrets/{secret_name}
 
 
+## Beware of how closely these two urls resemble each other
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/actions/secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+#                                                         ^^^^^^^ @@@@@@@                       ------------------------------------
+# versus:
+# https://docs.github.com/en/enterprise-cloud@latest/rest/codespaces/repository-secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+#                                                         ^^^^^^^^^^ @@@@@@@@@@@@@@@@@@                       ------------------------------------
+
+# They each have their own separate keys hence get-a-repository-codespaces-public-key.sh
+
+
 ts=$(date +%s)
-repo_secret_name="codespaces_secret_${ts}"
-repo_secret_value="pwr-codespaces-secret"
+repo_secret_name="pwr_codespaces_secret"
+repo_secret_value="pwr-codespaces-secret-phrase"
 
 secret_name=${repo_secret_name}
 key_id=$(./get-a-repository-codespaces-public-key.sh | jq -r '.key_id')
@@ -16,7 +27,6 @@ repository_id=$(./list-repo.sh $repo | jq -r '.id')
 
 
 json_file=tmp/repository-codespaces-secret.json
-
 jq -n \
            --arg secret_name "${secret_name}" \
            --arg key_id "${key_id}" \
@@ -28,9 +38,7 @@ jq -n \
            }' > ${json_file}
 
 
-set -x
 curl ${curl_custom_flags} \
-     -v \
      -X PUT \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
--- create-or-update-a-repository-secret.sh
+++ create-or-update-a-repository-secret.sh
@@ -1,17 +1,32 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/actions#create-or-update-a-repository-secret
-# PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}
+# https://docs.github.com/en/enterprise-cloud@latest/rest/codespaces/repository-secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+# PUT /repos/{owner}/{repo}/codespaces/secrets/{secret_name}
 
+
+## Beware of how closely these two urls resemble each other
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/actions/secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+#                                                         ^^^^^^^ @@@@@@@                       ------------------------------------
+# versus:
+# https://docs.github.com/en/enterprise-cloud@latest/rest/codespaces/repository-secrets?apiVersion=2022-11-28#create-or-update-a-repository-secret
+#                                                         ^^^^^^^^^^ @@@@@@@@@@@@@@@@@@                       ------------------------------------
+
+# They each have their own separate keys hence get-a-repository-codespaces-public-key.sh
+
+
+ts=$(date +%s)
+repo_secret_name="pwr_codespaces_secret"
+repo_secret_value="pwr-codespaces-secret-phrase"
 
 secret_name=${repo_secret_name}
-key_id=$(./get-a-repository-public-key.sh | jq -r '.key_id')
-repo_public_key=$(./get-a-repository-public-key.sh | jq -r '.key')
+key_id=$(./get-a-repository-codespaces-public-key.sh | jq -r '.key_id')
+repo_public_key=$(./get-a-repository-codespaces-public-key.sh | jq -r '.key')
 encrypted_value=$(ruby create-a-repository-secret-helper.rb ${repo_public_key} ${repo_secret_value})
 repository_id=$(./list-repo.sh $repo | jq -r '.id')
 
 
-json_file=tmp/repository-secret.json
+json_file=tmp/repository-codespaces-secret.json
 jq -n \
            --arg secret_name "${secret_name}" \
            --arg key_id "${key_id}" \
@@ -27,4 +42,5 @@ curl ${curl_custom_flags} \
      -X PUT \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/actions/secrets/${secret_name}" --data @${json_file}
+        "${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/codespaces/secrets/${secret_name}" --data @${json_file}
+
