8e6699801537e3cfc49ba6977f86412951a4beb5
David Morris <gm3dmo@github.com>
2025-02-13 08:16:53 +0000

    Changes to support dotcom-configure being smoother close #298

--- dotcom-configure.sh
+++ dotcom-configure.sh
@@ -19,23 +19,23 @@ python3 configure.py --hostname "${hostname}" \
                      --enterprise-name "${enterprise_name}" \
                      --org "${org}" \
                      --repo "${repo}" \
+                     --default-repo-visibility ${default_repo_visibility:-private} \
                      --token "${github_token}" \
                      --webhook-url "${webhook}" \
                      --configure-app yes \
                      --app-id "${app_id}" \
-                     --app-client-secret "${app_client_secret}" \
-                     --installation-id "${installation_id}" \
-                     --client-id "${client_id}" \
-                     --team-members "${team_members}" \
-                     --team-admin "${team_admin}" \
-                     --default-committer "${default_committer}" \
-                     --private-pem-file "${private_pem_file}" \
-                     --pr-approver-name "${pr_approver_name}" \
-                     --pr-approver-token "${pr_approver_token}" \
-                     --chrome-profile "${chrome_profile}" \
-                     --x-client-id "${x_client_id}" \
-                     --x-client-secret "${x_client_secret}" \
-                     --default-repo-visibility ${default_repo_visibility} \
+                     --app-client-secret "${app_client_secret:-an_app_client_secret}" \
+                     --installation-id "${installation_id:-an_app_installation_id}" \
+                     --client-id "${client_id:-an_app_client_id}" \
+                     --team-members "${team_members:-team_members_space_separated}" \
+                     --team-admin "${team_admin:-a_team_admin}" \
+                     --default-committer "${default_committer:-a_default_committer}" \
+                     --private-pem-file "${private_pem_file:-a_private_pem_file}" \
+                     --pr-approver-name "${pr_approver_name:-a_username}" \
+                     --pr-approver-token "${pr_approver_token:-a_fine_grained_pat}" \
+                     --chrome-profile "${chrome_profile:-a_chrome_profile_number}" \
+                     --x-client-id "${x_client_id:-an_ouath_app_client_id}" \
+                     --x-client-secret "${x_client_secret:-an_oauth_client_secret}" \
                      --enterprise-app-name "${ent_app_name:-an_enterprise_app_name}" \
                      --enterprise-app-id "${ent_app_id:-an_enterprise_app_id}" \
                      --enterprise-app-installation-id "${ent_app_installation_id:-an_enterprise_app_installation_id}" \
