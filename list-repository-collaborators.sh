0d0a74ca3bc00a268b08beb785f5cea26f16b7f0
David Morris <gm3dmo@github.com>
2025-02-15 21:21:49 +0000

    Adding tidy updates

--- list-repository-collaborators.sh
+++ list-repository-collaborators.sh
@@ -1,13 +1,22 @@
 .   ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/collaborators#list-repository-collaborators
+# https://docs.github.com/en/enterprise-cloud@latest/rest/collaborators/collaborators?apiVersion=2022-11-28
 # GET /repos/{owner}/{repo}/collaborators
 
-#affiliation="all"
-#affiliation="direct"
-affiliation="outside"
 
+if [ -z "$1" ]
+  then
+    affiliation="outside"
+    #affiliation="all"
+    #affiliation="direct"
+  else
+   affiliation=$1
+fi
+
+
+set -x
 curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/collaborators?affiliation=${affiliation}"
