6109a534d7f6a48169b38a8bf1d91c82db89397b
David Morris <gm3dmo@github.com>
2025-02-15 21:23:43 +0000

    UPdate remove an organization member

--- remove-an-organization-member.sh
+++ remove-an-organization-member.sh
@@ -1,6 +1,6 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/orgs#remove-an-organization-member
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/members?apiVersion=2022-11-28#remove-an-organization-member
 # DELETE /orgs/{org}/members/{username}
 
 if [ -z "$1" ]
@@ -13,6 +13,9 @@ fi
 
 curl ${curl_custom_flags} \
      -X DELETE \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/orgs/${org}/member/${username}
+        "${GITHUB_API_BASE_URL}/orgs/${org}/member/${username}"
+
+
