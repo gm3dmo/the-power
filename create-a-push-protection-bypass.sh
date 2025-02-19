b126a0cb48023d9706a83f403aeae0fda730d464
David Morris <gm3dmo@github.com>
2025-02-19 22:15:50 +0000

    Adding create a push protection bypass script

--- create-a-push-protection-bypass.sh
+++ create-a-push-protection-bypass.sh
@@ -3,7 +3,7 @@
 # https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning/secret-scanning?apiVersion=2022-11-28#create-a-push-protection-bypass
 # POST /repos/{owner}/{repo}/secret-scanning/push-protection-bypasses
 
-# Use create-a-blob.sh to get a placeholder_id
+# Use create-blob.sh to get a placeholder_id (for programmatic) or use the placeholder ids from the list when doing git push.
 
 # If the script is passed an argument $1 use that as the name
 if [ -z "$1" ]
@@ -12,7 +12,6 @@ if [ -z "$1" ]
   else
     placeholder_id=$1
 fi
-
 
 reason="will_fix_later"
 
@@ -31,3 +30,4 @@ curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/secret-scanning/push-protection-bypasses"  --data @${json_file}
+
