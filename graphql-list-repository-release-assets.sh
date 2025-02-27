be98ae79eebd175a7949b4fc7152291614fa24f9
David Morris <gm3dmo@github.com>
2025-02-27 13:51:49 +0000

    Adding a graphql script

---
+++ graphql-list-repository-release-assets.sh
@@ -0,0 +1,55 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/graphql/reference/objects#repository
+
+read -r -d '' graphql_script <<- EOF
+{
+search(type: REPOSITORY, query: "repo:$org/$repo", first: 1) {
+    nodes {
+      ... on Repository {
+        id
+        databaseId
+        nameWithOwner
+        createdAt
+        isSecurityPolicyEnabled
+        isArchived
+        rulesets(first: 10) {
+          totalCount
+          nodes {
+            name
+            target
+            enforcement
+          }
+        }
+        releases(last: 1) {
+          totalCount
+          nodes {
+            tagName
+            releaseAssets(first: 100) {
+              totalCount
+              nodes {
+                name
+                size
+                createdAt
+                updatedAt
+                downloadUrl
+                contentType
+                downloadCount
+                uploadedBy { name }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+EOF
+
+graphql_script="$(echo ${graphql_script//\"/\\\"})"
+
+curl ${curl_custom_flags} \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H 'Accept: application/vnd.github.audit-log-preview+json' \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        ${GITHUB_APIV4_BASE_URL} -d "{ \"query\": \"$graphql_script\"}"
