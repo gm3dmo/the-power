3895fa9433db9e20cc9ef5a032911f4107c25693
David Morris <gm3dmo@github.com>
2025-03-05 13:38:17 +0000

    Delete an organization webhook auto mode

--- delete-an-organization-webhook.sh
+++ delete-an-organization-webhook.sh
@@ -1,12 +1,12 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/orgs/webhooks?#delete-an-organization-webhook
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/webhooks?apiVersion=2022-11-28#delete-an-organization-webhook
 # DELETE /orgs/{org}/hooks/{hook_id}
 
 
 if [ -z "$1" ]
   then
-    hook_id=1
+    hook_id=$(./list-organization-webhooks.sh  jq '.[-1].id')
   else
     hook_id=$1
 fi
@@ -16,4 +16,5 @@ curl ${curl_custom_flags} \
      -X DELETE \
      -H "Authorization: token ${GITHUB_TOKEN}" \
      -H "Accept: application/vnd.github.v3+json" \
-        ${GITHUB_API_BASE_URL}/orgs/${org}/hooks/${hook_id}
+        "${GITHUB_API_BASE_URL}/orgs/${org}/hooks/${hook_id}"
+
