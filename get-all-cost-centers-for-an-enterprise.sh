ba9c663f1f4fe25e98d41a056e6e8f8cbb49734d
David Morris <gm3dmo@github.com>
2025-06-04 11:28:00 +0100

    Adding get all cost centers for an enterprise script close #414

--- get-all-cost-centers-for-an-enterprise.sh
+++ get-all-cost-centers-for-an-enterprise.sh
@@ -4,9 +4,18 @@
 # GET /enterprises/{enterprise}/settings/billing/cost-centers
 
 
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    enterprise=$enterprise
+  else
+    enterprise=$1
+fi
+
 
 curl ${curl_custom_flags} \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/settings/billing/cost-centers"
+        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/settings/billing/cost-centers" 
+
