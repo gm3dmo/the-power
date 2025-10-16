8a0fb434b9b31323d04defd5d403d027b2032472
David Morris <gm3dmo@github.com>
2025-10-16 12:52:25 +0100

    Add a fix for broken url. Close #472

--- create-a-review-comment-for-a-pull-request.sh
+++ create-a-review-comment-for-a-pull-request.sh
@@ -45,5 +45,5 @@ curl -v ${curl_custom_flags} \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/pulls/${pull_number}/comments --data @${json_file}"
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/pulls/${pull_number}/comments" --data @${json_file}
 
