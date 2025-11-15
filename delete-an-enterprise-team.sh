a328443a0a5d8be5e8f6e5d760899255fe4944c3
David Morris <gm3dmo@github.com>
2025-11-15 08:51:44 +0000

    Adding delete an enterprise team script close #487

---
+++ delete-an-enterprise-team.sh
@@ -0,0 +1,22 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/enterprise-teams/enterprise-teams?apiVersion=2022-11-28#delete-an-enterprise-team
+# DELETE /enterprises/{enterprise}/teams/{team_slug}
+
+
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    team_slug=$enterprise_team_name
+  else
+    team_slug=$1
+fi
+
+
+curl ${curl_custom_flags} \
+     -X DELETE \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/teams/${team_slug}"
+
