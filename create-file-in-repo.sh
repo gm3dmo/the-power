f90adf033a509cdbb803a841d6aea630b0d2d4b9
David Morris <gm3dmo@github.com>
2025-11-18 16:45:06 +0000

    test

--- create-file-in-repo.sh
+++ create-file-in-repo.sh
@@ -1,11 +1,15 @@
 .  ./.gh-api-examples.conf
 
+set -x
 # https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
 # PUT /repos/:owner/:repo/contents/:path
 
+for r in "repo-a" "repo-b" "repo-c" "repo-d"
+do
+repo=$r
 
 filename_in_repo="docs/README.md"
-json_file_to_upload="tmp/README.json"
+json_file_to_upload="tmp/create-commit-readme.json"
 
 contents_path="contents/${filename_in_repo}"
 
@@ -15,3 +19,4 @@ curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/${contents_path} --data @${json_file_to_upload}
+done
