e7c78781785a70ad3f7e4e26bbe3cde2cf4a54e5
David Morris <gm3dmo@github.com>
2025-12-18 10:36:58 +0000

    Adding get-all-organization-roles-for-an-organization.sh closes #497

--- get-all-organization-roles-for-an-organization.sh
+++ get-all-organization-roles-for-an-organization.sh
@@ -1,13 +1,13 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/orgs/organization-roles?apiVersion=2022-11-28#get-all-organization-roles-for-an-organization
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/organization-roles?apiVersion=2022-11-28#get-all-organization-roles-for-an-organization
 # GET /orgs/{org}/organization-roles
 
 
 # If the script is passed an argument $1 use that as the name
 if [ -z "$1" ]
   then
-    org=$org
+   org=$org
   else
     org=$1
 fi
@@ -18,4 +18,5 @@ curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "${GITHUB_API_BASE_URL}/orgs/${org}/organization-roles"
+
 
