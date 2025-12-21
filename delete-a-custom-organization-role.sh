61b5e1851f91181cf3d81cf12f21217b4856706d
David Morris <gm3dmo@github.com>
2025-12-21 10:24:58 +0000

    delete-a-custom-organization-role.sh to close #508

---
+++ delete-a-custom-organization-role.sh
@@ -0,0 +1,15 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/enterprise-cloud@latest/rest/orgs/organization-roles?apiVersion=2022-11-28#delete-a-custom-organization-role
+# DELETE /orgs/{org}/organization-roles/{role_id}
+
+
+role_id=$1
+
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/orgs/${org}/organization-roles/${role_id}" 
+
