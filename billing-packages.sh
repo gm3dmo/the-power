5e9e07c443a480c70be54c6da5479ac36277046b
David Morris <gm3dmo@github.com>
2026-01-11 08:03:51 +0000

    Removing scripts for billing as its closing https://github.blog/changelog/2025-09-26-product-specific-billing-apis-are-closing-down/ close #469

--- billing-packages.sh
+++
@@ -1,9 +0,0 @@
-.  ./.gh-api-examples.conf
-
-# https://docs.github.com/en/rest/reference/billing#get-github-packages-billing-for-an-organization
-# GET /orgs/{org}/settings/billing/packages
-
-curl ${curl_custom_flags} \
-     -H "Accept: application/vnd.github.v3+json" \
-     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/orgs/{$org}/settings/billing/packages
