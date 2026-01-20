909a974d574730214f0e8ab16413743f8fc3df5d
David Morris <gm3dmo@github.com>
2026-01-20 19:47:33 +0000

    Make mailserver work with python3

--- extras/mail-server/mail-server.py
+++ extras/mail-server/mail-server.py
@@ -1,25 +1,27 @@
 from datetime import datetime
-import asyncore
-from smtpd import SMTPServer
+from aiosmtpd.controller import Controller
+from aiosmtpd.handlers import AsyncMessage
+import asyncio
 
-class EmlServer(SMTPServer):
+class EmlHandler(AsyncMessage):
     no = 0
-    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
-        filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'),
-            self.no)
-        print(filename)
-        f = open(filename, 'wb')
-        f.write(data)
-        f.close
+
+    async def handle_message(self, message):
+        filename = '%s-%d.eml' % (datetime.now().strftime('%Y%m%d%H%M%S'), self.no)
+        with open(filename, 'wb') as f:
+            f.write(message.as_bytes())
         print('%s saved.' % filename)
         self.no += 1
 
 def run():
-    EmlServer(('0.0.0.0', 10025), None)
+    controller = Controller(EmlHandler(), hostname='0.0.0.0', port=10025)
+    controller.start()
+    print("SMTP server running on port 10025...")
     try:
-        asyncore.loop()
+        asyncio.get_event_loop().run_forever()
     except KeyboardInterrupt:
-        pass
+        controller.stop()
+
 
 if __name__ == '__main__':
     run()
