f213feadd0a816aaedd3a2130ae0d32f2a90f8e6
David Morris <gm3dmo@github.com>
2026-04-12 10:13:39 +0100

    Fixing collaborator checks

--- add-collaborator-to-repo.sh
+++ add-collaborator-to-repo.sh
@@ -1,4 +1,4 @@
-.   ./.gh-api-examples.conf
+.  ./.gh-api-examples.conf
 
 # https://docs.github.com/en/rest/collaborators/collaborators?apiVersion=2022-11-28#add-a-repository-collaborator
 # PUT /repos/{owner}/{repo}/collaborators/{username}
@@ -6,15 +6,29 @@
 # limits: https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-personal-account-on-github/managing-access-to-your-personal-repositories/inviting-collaborators-to-a-personal-repository
 
 
-username=${repo_collaborator}
+# If the script is passed an argument $1 use that as the name
+if [ -z "$1" ]
+  then
+    username=${repo_collaborator}
+  else
+    username=$1
+fi
+
 permission=${2:-push}
 
-JSON_TEMPLATE='{"permission":"%s"}'
-JSON_DATA=$(printf "$JSON_TEMPLATE" "$permission")
+json_file=tmp/add-collaborator-to-repo.json
+
+jq -n \
+           --arg permission "${permission}" \
+           '{
+             permission : $permission,
+           }' > ${json_file}
+
 
 curl ${curl_custom_flags} \
      -X PUT \
-     -H "Accept: application/vnd.github.v3+json" \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/collaborators/${username}" -d ${JSON_DATA}
+        "${GITHUB_API_BASE_URL}/repos/${org}/${repo}/collaborators/${username}" --data @${json_file}
 
--- check-if-a-user-is-a-repository-collaborator.sh
+++ check-if-a-user-is-a-repository-collaborator.sh
@@ -1,7 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/enterprise-cloud@latest/rest/collaborators/collaborators?apiVersion=2022-11-28#remove-a-repository-collaborator
-# DELETE /repos/{owner}/{repo}/collaborators/{username}
+# https://docs.github.com/en/rest/collaborators/collaborators?apiVersion=2022-11-28#check-if-a-user-is-a-repository-collaborator
+# GET /repos/{owner}/{repo}/collaborators/{username}
 
 
 if [ -z "$1" ]
@@ -12,10 +12,9 @@ if [ -z "$1" ]
 fi
 
 
+set -x
 curl ${curl_custom_flags} \
-     -X DELETE \
      -H "X-GitHub-Api-Version: ${github_api_version}" \
-     -H "Accept: application/vnd.github.v3+json" \
+     -H "Accept: application/vnd.github+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "${GITHUB_API_BASE_URL}/repos/${owner}/${repo}/collaborators/${username}"
-
