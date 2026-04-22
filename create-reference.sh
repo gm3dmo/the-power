4f7cee52d9f8779636653a0c27d0234e3da90b33
David Morris <gm3dmo@github.com>
2026-04-22 07:29:33 +0100

    Removing old json references

--- create-reference.sh
+++ create-reference.sh
@@ -1,7 +1,7 @@
 .  ./.gh-api-examples.conf
 
-# https://docs.github.com/en/rest/reference/git#create-a-reference
-# POST /repos/:owner/:repo/git/refs
+# https://docs.github.com/en/enterprise-cloud@latest/rest/git/refs?apiVersion=2026-03-10#create-a-reference
+# POST /repos/{owner}/{repo}/git/refs
 
 sha=$(curl --silent -H "Authorization: Bearer ${GITHUB_TOKEN}" ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/ref/heads/${base_branch} | jq -r '.object.sha')
 
@@ -12,19 +12,15 @@ if [ -z "$1" ]
     ref=$1
 fi
 
-json=tmp/create-reference.json
-
-rm -f ${json}
-
+json_file=tmp/create-a-reference.json
 jq -n \
         --arg ref "${ref}" \
         --arg sha "${sha}" \
           '{ ref: $ref,
-             sha: $sha }' > ${json}
+             sha: $sha }' > ${json_file}
 
 curl ${curl_custom_flags} \
      -H "Accept: application/vnd.github.v3+json" \
      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs --data @${json}
+        ${GITHUB_API_BASE_URL}/repos/${org}/${repo}/git/refs --data @${json_file}
 
-rm -f ${json}
