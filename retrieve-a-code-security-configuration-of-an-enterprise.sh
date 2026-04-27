40d7bff2aa7be9e51fcd24697148a2de5cc8408e
David Morris <gm3dmo@github.com>
2026-04-27 10:57:29 +0100

    Adding code security endpoints

---
+++ retrieve-a-code-security-configuration-of-an-enterprise.sh
@@ -0,0 +1,19 @@
+.  ./.gh-api-examples.conf
+
+# https://docs.github.com/en/rest/code-security/configurations?apiVersion=2026-03-10#retrieve-a-code-security-configuration-of-an-enterprise
+# GET /enterprises/{enterprise}/code-security/configurations/{configuration_id}
+
+if [ -z "$1" ]
+  then
+    enterprise=${enterprise}
+  else
+    enterprise=$1
+fi
+
+configuration_id=$(./get-code-security-configurations-for-an-enterprise.sh | jq '[.[].id] | max')
+
+curl ${curl_custom_flags} \
+     -H "X-GitHub-Api-Version: ${github_api_version}" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_API_BASE_URL}/enterprises/${enterprise}/code-security/configurations/${configuration_id}"
